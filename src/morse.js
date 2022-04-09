819f42ac5f6f918579c8321f5f90b2d3de810f0c
Randy Loeb <githubrandyloeb@randonloeb.com>
2022-04-09 02:53:32 -0400

    comment

--- src/morse.js
+++ src/morse.js
@@ -3,6 +3,8 @@ import ko from 'knockout'
 // see https://getbootstrap.com/docs/5.0/getting-started/webpack/
 import 'bootstrap/dist/css/bootstrap.min.css'
 // You can specify which plugins you need
+// Note that even though these don't seem to be used directly,
+// they are used by the accordian.
 // eslint-disable-next-line no-unused-vars
 import { Tooltip, Toast, Popover } from 'bootstrap'
 
