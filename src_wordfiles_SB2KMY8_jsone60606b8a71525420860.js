80b649e4c96704d77e9f45828663c6db4fb4e822
RandyLoeb <34562250+RandyLoeb@users.noreply.github.com>
2022-04-25 15:25:21 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@9e4d10907b00b9b082a715d75a81887b5bd114b3 🚀

---
+++ src_wordfiles_SB2KMY8_jsone60606b8a71525420860.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_SB2KMY8_json"],{
+
+/***/ "./src/wordfiles/SB2KMY8.json":
+/*!************************************!*\
+  !*** ./src/wordfiles/SB2KMY8.json ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"letters":"reatinpgslcdhofuwbkmy4028bk","minWordSize":3,"maxWordSize":3,"practiceSeconds":120}');
+
+/***/ })
+
+}]);
