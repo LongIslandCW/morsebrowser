5d8c1f237be6bf38ef7ce32716cb72b1131ef6aa
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-04-25 04:39:06 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@34d92163b7f7fa2adf41756114c02f96ff21cb7c 🚀

---
+++ dev/src_wordfiles_SB2KMY11_json861336361aa9383e02ce.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_SB2KMY11_json"],{
+
+/***/ "./src/wordfiles/SB2KMY11.json":
+/*!*************************************!*\
+  !*** ./src/wordfiles/SB2KMY11.json ***!
+  \*************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"letters":"reatinpgslcdhofuwbkmy4028bkzj/16.<ar><sk><bt>","minWordSize":3,"maxWordSize":3,"practiceSeconds":120}');
+
+/***/ })
+
+}]);
