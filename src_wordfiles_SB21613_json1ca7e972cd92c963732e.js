877b1144bce88a390b318261face025f30ac83ab
RandyLoeb <34562250+RandyLoeb@users.noreply.github.com>
2022-04-26 04:09:59 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@992d5bb87903b982c6aa1efd964e999f7451be66 🚀

---
+++ src_wordfiles_SB21613_json1ca7e972cd92c963732e.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_SB21613_json"],{
+
+/***/ "./src/wordfiles/SB21613.json":
+/*!************************************!*\
+  !*** ./src/wordfiles/SB21613.json ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"letters":"reatinpgslcdhofuwb16.<ar><sk><bt>73?qxv59,kmy40","minWordSize":3,"maxWordSize":3,"practiceSeconds":120}');
+
+/***/ })
+
+}]);
---
+++ src_wordfiles_SB2QXV10_json36bffa30040c1e56b17e.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_SB2QXV10_json"],{
+
+/***/ "./src/wordfiles/SB2QXV10.json":
+/*!*************************************!*\
+  !*** ./src/wordfiles/SB2QXV10.json ***!
+  \*************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"letters":"reatinpgslcdhofuwbqxv59,kmy4028bk","minWordSize":3,"maxWordSize":3,"practiceSeconds":120}');
+
+/***/ })
+
+}]);
