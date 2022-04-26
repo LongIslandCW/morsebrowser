450446d44eb909ea27b0619de0864d9adc05a036
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-04-26 02:59:34 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@4161097082d0109d93cceb6aea05be2808b4bb92 🚀

---
+++ dev/src_wordfiles_SB2409_jsondde551ea5cc05427f018.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_SB2409_json"],{
+
+/***/ "./src/wordfiles/SB2409.json":
+/*!***********************************!*\
+  !*** ./src/wordfiles/SB2409.json ***!
+  \***********************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"letters":"reatinpgslcdhofuwb4028bkzj/","minWordSize":3,"maxWordSize":3,"practiceSeconds":120}');
+
+/***/ })
+
+}]);
