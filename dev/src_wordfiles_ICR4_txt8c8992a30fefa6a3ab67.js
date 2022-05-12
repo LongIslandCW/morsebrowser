57ca8264e470af0c1fe0f6f730f4b178bfea8239
MikePadron <103018958+MikePadron@users.noreply.github.com>
2022-05-12 19:39:59 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@0dc100a63f4de3ba6693b09c43dd8da7834d81d5 🚀

---
+++ dev/src_wordfiles_ICR4_txt8c8992a30fefa6a3ab67.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_ICR4_txt"],{
+
+/***/ "./src/wordfiles/ICR4.txt":
+/*!********************************!*\
+  !*** ./src/wordfiles/ICR4.txt ***!
+  \********************************/
+/***/ ((module) => {
+
+module.exports = "\n";
+
+/***/ })
+
+}]);
