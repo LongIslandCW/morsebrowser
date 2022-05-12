31bca6be9905983a9d84a8aff750abac958637b8
MikePadron <103018958+MikePadron@users.noreply.github.com>
2022-05-12 19:39:29 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@d6c77bdb558cefd90dd331edf95f5b2f9fc5e402 🚀

---
+++ dev/src_wordfiles_ICR2_txt2679af50e61de178940d.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_ICR2_txt"],{
+
+/***/ "./src/wordfiles/ICR2.txt":
+/*!********************************!*\
+  !*** ./src/wordfiles/ICR2.txt ***!
+  \********************************/
+/***/ ((module) => {
+
+module.exports = "\n";
+
+/***/ })
+
+}]);
