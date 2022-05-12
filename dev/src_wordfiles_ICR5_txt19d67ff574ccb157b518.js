3f6689c9b7a5f1fe916c79507820da46684dc863
MikePadron <103018958+MikePadron@users.noreply.github.com>
2022-05-12 19:40:06 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@a151d59d7f229e4cf34ae2cf8017303c16d0f0b2 🚀

---
+++ dev/src_wordfiles_ICR5_txt19d67ff574ccb157b518.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_ICR5_txt"],{
+
+/***/ "./src/wordfiles/ICR5.txt":
+/*!********************************!*\
+  !*** ./src/wordfiles/ICR5.txt ***!
+  \********************************/
+/***/ ((module) => {
+
+module.exports = "\n";
+
+/***/ })
+
+}]);
