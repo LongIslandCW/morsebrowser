37d83eaf3f716b40c2b256c349ab5e8bed5fccf5
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-05-15 18:50:38 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@e71e914e5909b4c6b4f70e7ee88ef9f74351da01 🚀

---
+++ dev/src_wordfiles_ICR5_txt2c5caf5ffc618f872fc5.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_ICR5_txt"],{
+
+/***/ "./src/wordfiles/ICR5.txt":
+/*!********************************!*\
+  !*** ./src/wordfiles/ICR5.txt ***!
+  \********************************/
+/***/ ((module) => {
+
+module.exports = "A\nI\nAT\nIT\nNO\nTO\nAS\nSO\nOR\nAN\nON\nIN\nIS\nANT\nTOE\nONE\nRAT\nSAT\nEAT\nSIT\nCAR\nACE\nCAT\nOAR\nSET\nARE\nSEE\nACT\nTAN\nRAN\nEEL\nNOR\nTIN\nATE\nCAN\nOIL\nILL\nLOT\nCOT\nNOT\nTEN\nLIE\nALL\nITS\nLATE\nTORN\nEARN\nSAIL\nCOIN\nTALE\nRARE\nLOST\nTOOL\nROLL\nCOOT\nSOLE\nSTAN\nASIA\nROSE\nLOSE\nACRE\nONCE\nTONE\nLESS\nTEST\nCATS\nSORT\nLACE\nNEAR\nROOT\nCENT\nRATS\nCANT\nREST\nSLOT\nLOON\nEATS\nNEAT\nELSE\nALSO\nTILT\nCORE\nLETS\nLAST\nCOST\nSOOT\nSINE\nRENT\nISNT\nIOTA\nNOON\nACES\nNOTE\nCARS\nCOAT\nREEL\nCOAL\nEASE\nSARA\nCONE\nIRON\nRACE\nRISE\nCASE\nSOIL\nNICE\nSEEN\nSORE\nTELL\nSCAN\nTIRE\nRANT\nSELL\nCOOL\nNEON\nLEAN\nTOES\nLINE\nCORN\nOATS\nAREA\nSEAL\nSOON\nCITE\nOARS\nREAR\nTOSS\nLION\nTALL\nTART\nCOIL\nNOSE\nLOTS\nSALE\nSANE\nTENT\nLONE\nCARE\nRAIL\nCANE\nTEAR\nTREE\nTOTE\nCANS\nLOAN\nLENS\nLIST\nSEAT\nSTAR\nRAIN\nNAIL\nROAR\nREAL\nRIOT\nTAIL\n\n";
+
+/***/ })
+
+}]);
