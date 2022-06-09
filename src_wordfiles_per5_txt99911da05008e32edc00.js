71f7ee667e67ff4d65917c09cfdd7e24c6f7f0c5
RandyLoeb <34562250+RandyLoeb@users.noreply.github.com>
2022-06-09 21:26:49 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@478d1d18b1392c17bac66c0f299fb507cc4e4ed0 🚀

---
+++ src_wordfiles_per5_txt99911da05008e32edc00.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_per5_txt"],{
+
+/***/ "./src/wordfiles/per5.txt":
+/*!********************************!*\
+  !*** ./src/wordfiles/per5.txt ***!
+  \********************************/
+/***/ ((module) => {
+
+module.exports = "V V R R TNX FER CALL UR RST 348 348 QTH NR AUSTIN, TX\n";
+
+/***/ })
+
+}]);
