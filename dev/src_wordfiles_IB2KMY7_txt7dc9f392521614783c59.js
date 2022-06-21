67cbe432c8911b03221bd7aa3b988adfbacd8363
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-06-21 07:24:29 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@5b497c23256d1f4c598cb89c41d84512197a32af 🚀

---
+++ dev/src_wordfiles_IB2KMY7_txt7dc9f392521614783c59.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_IB2KMY7_txt"],{
+
+/***/ "./src/wordfiles/IB2KMY7.txt":
+/*!***********************************!*\
+  !*** ./src/wordfiles/IB2KMY7.txt ***!
+  \***********************************/
+/***/ ((module) => {
+
+module.exports = "MY RIG KNWD\nANT DIPOLE\nANT YAGI\nOK OM SOLID CPY\nSOLID CPI\nFB CATHY\nNAME MIKE\nNAME HOWARD\nNAME RICH\nBEEN HAM TWO YRS\nSRI OM\nMY RIG ICOM\nMY RIG YAESU\nMY RIG TEN TEC\nMY RIG BOAT ANCHOR\nMY KEY BUG\nNICE SIG OM\n";
+
+/***/ })
+
+}]);
