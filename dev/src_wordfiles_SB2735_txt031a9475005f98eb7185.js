ac046b56f53c208a03b41fa5681583fa5ff1ba2a
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-06-22 06:10:41 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@b5d2eac5fb9a7be85aa9345ea2740fd684eb4fb0 🚀

--- dev/src_wordfiles_SB2735_txt031a9475005f98eb7185.js
+++ dev/src_wordfiles_SB2735_txt031a9475005f98eb7185.js
@@ -7,7 +7,7 @@
   \**********************************/
 /***/ ((module) => {
 
-module.exports = "AC3D N3GE N3IOE N7RD AB7NT K7MY KB7YM N3KM WB3KMY WB3WUB WB3OHF KB3ERA AA7TIN AC7CLD AB7CDL WB3EAR N3FOH N7CDL";
+module.exports = "AC3D N3GE N3IOE N7RD AB7NT WB3WUB WB3OHF AA7TIN AC7CLD AB7CDL WB3EAR N3FOH N7CDL  \n";
 
 /***/ })
 
