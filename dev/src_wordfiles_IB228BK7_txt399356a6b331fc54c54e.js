7ef8e2f56013355724b7492386feaf70cd1461aa
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-06-22 07:33:14 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@fc3420fb986dea780b43b2048ce72651dbca1ff2 🚀

--- dev/src_wordfiles_IB228BK7_txt399356a6b331fc54c54e.js
+++ dev/src_wordfiles_IB228BK7_txt399356a6b331fc54c54e.js
@@ -7,7 +7,7 @@
   \************************************/
 /***/ ((module) => {
 
-module.exports = "OK HOWARD BTU BK \nBK FB RICH AGE 28 RETIRED HI HI BK \nBK FB ES TU FER INFO UR RIG ES ANT DOING FB WID GUD SIG HR BK \nBK ANT EFHW IN ATTIC BK\n";
+module.exports = "OK HOWARD BTU BK \nBK FB RICH AGE 28 RETIRED HI HI BK \nBK FB ES TU FER INFO UR RIG ES ANT DOING FB WID GUD SIG HR BK \nBK ANT EFHW IN ATTIC BK\n\n";
 
 /***/ })
 
