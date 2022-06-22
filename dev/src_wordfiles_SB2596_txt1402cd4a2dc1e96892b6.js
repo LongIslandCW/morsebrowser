3c01bbbe27211b8b6598aefd44bca52735986304
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-06-22 06:00:43 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@ce41b19c6e4122d6898d76561381a68e6a7466d5 🚀

--- dev/src_wordfiles_SB2596_txt1402cd4a2dc1e96892b6.js
+++ dev/src_wordfiles_SB2596_txt1402cd4a2dc1e96892b6.js
@@ -7,7 +7,7 @@
   \**********************************/
 /***/ ((module) => {
 
-module.exports = "AL AR CA CO CT DE FL GA HI ID IL IN IA LA NE NH NC ND OH PA RI SC SD TN UT WA WI";
+module.exports = "AL AR CA CO CT DE FL GA HI ID IL IN IA LA NE NH NC ND OH PA RI SC SD TN UT WA WI\n";
 
 /***/ })
 
