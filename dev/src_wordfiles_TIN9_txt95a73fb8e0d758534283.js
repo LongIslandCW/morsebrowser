1dc947ee0fb427f64d05dde7d83c436d4b0a05b6
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-06-22 09:30:25 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@f17246eed24a1fc702528a45e022d683a0218266 🚀

---
+++ dev/src_wordfiles_TIN9_txt95a73fb8e0d758534283.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_TIN9_txt"],{
+
+/***/ "./src/wordfiles/TIN9.txt":
+/*!********************************!*\
+  !*** ./src/wordfiles/TIN9.txt ***!
+  \********************************/
+/***/ ((module) => {
+
+module.exports = "BUT NEW EAT WET AIR TUNE TUNER TIN WIRE WENT BURN TEN ART WATER TRAIN NUT BAIT RENT\n";
+
+/***/ })
+
+}]);
