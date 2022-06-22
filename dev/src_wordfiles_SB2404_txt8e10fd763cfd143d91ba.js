7d74c2ffd16fde6755283ffcaddc7dfad346c310
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-06-22 06:52:23 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@a9b29f20a61431378f07792225daedd2ee2c64d8 🚀

--- dev/src_wordfiles_SB2404_txt8e10fd763cfd143d91ba.js
+++ dev/src_wordfiles_SB2404_txt8e10fd763cfd143d91ba.js
@@ -7,7 +7,7 @@
   \**********************************/
 /***/ ((module) => {
 
-module.exports = "ANT DIPOLE UP 40 FT <BT> AGE 40 <BT> NICE TO CU AGN ON 40 <BT> NICE SIG ON 40 <BT> AD0WE DE AI4PL <BT> G0POT DE WB0ISG GA MIKE NICE SIG ON 40";
+module.exports = "ANT DIPOLE UP 4Ø FT\nAGE 4Ø\nNICE TO CU AGN ON 4Ø\nNICE SIG ON 4Ø\nADØWE DE AI4PL\nGØPOT DE WBØISG GA NED NICE SIG ON 4Ø  \n";
 
 /***/ })
 
