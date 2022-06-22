d34a34aae7e17ec19c1c5afc2ff3d34f1706a2cd
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-06-22 09:52:58 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@69e28d2076c16535b715a66329884e1957740074 🚀

---
+++ dev/src_wordfiles_PGS10_txt68c46affee84a1e3e1ed.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_PGS10_txt"],{
+
+/***/ "./src/wordfiles/PGS10.txt":
+/*!*********************************!*\
+  !*** ./src/wordfiles/PGS10.txt ***!
+  \*********************************/
+/***/ ((module) => {
+
+module.exports = "PIE RAG SIP PIN TAP PEA GAP PEG PIG GRIP NAP PAN PING GASP PAGE GRASP SPIN PAIN\n";
+
+/***/ })
+
+}]);
