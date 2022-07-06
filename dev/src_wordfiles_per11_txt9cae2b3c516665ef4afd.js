6b7d57e8e1717423702e3596d861f73891cf356c
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-07-06 14:02:50 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@9ee91b0b6318b882487495c4ae1b067f06d94717 🚀

---
+++ dev/src_wordfiles_per11_txt9cae2b3c516665ef4afd.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_per11_txt"],{
+
+/***/ "./src/wordfiles/per11.txt":
+/*!*********************************!*\
+  !*** ./src/wordfiles/per11.txt ***!
+  \*********************************/
+/***/ ((module) => {
+
+module.exports = "4VV4SH\nJW7Z8Z\nCYHV45\n4VV4SH\n";
+
+/***/ })
+
+}]);
