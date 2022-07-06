bc623d9720583a91af85312352b973d53a00227d
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-07-06 14:20:17 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@b7b61fc3488ffd3cdb8e7096dbe9293c06f76a57 🚀

--- dev/src_wordfiles_per7_txtbc08453e5f3ea3165653.js
+++ dev/src_wordfiles_per7_txtbc08453e5f3ea3165653.js
@@ -7,7 +7,7 @@
   \********************************/
 /***/ ((module) => {
 
-module.exports = "QRX 5 QRX 5 STBY PHONE CALL SRI ARBK\n";
+module.exports = "QRX 5 QRX 5 STBY PHONE CALL SRI <AR> BK\n";
 
 /***/ })
 
