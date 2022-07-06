f4424f3e0c5c678764e0a6d4f5e8a75c610dd3ad
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-07-06 14:21:28 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@a5e5b4901f86e80a3ca3d851987a211ebc6fa812 🚀

--- dev/src_wordfiles_per5_txtdeeb1799cf7c5ee4a64f.js
+++ dev/src_wordfiles_per5_txtdeeb1799cf7c5ee4a64f.js
@@ -7,7 +7,7 @@
   \********************************/
 /***/ ((module) => {
 
-module.exports = "RR TNX FER CALL UR RST 348 348 QTH NR AUSTIN, TX <AR> K\n";
+module.exports = "RR TNX FER CALL UR RST 348 348 QTH NR AUSTIN, TX <AR> BK\n";
 
 /***/ })
 
