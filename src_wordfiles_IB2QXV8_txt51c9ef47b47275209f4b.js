cf0d70f9dd9a4a13a79c75fb86d5b7f455f497fd
RandyLoeb <34562250+RandyLoeb@users.noreply.github.com>
2022-07-18 23:12:58 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@8a7dea70c2d49cf01d6afc0d17fbdeb5112fef28 🚀

--- src_wordfiles_IB2QXV8_txt51c9ef47b47275209f4b.js
+++ src_wordfiles_IB2QXV8_txt51c9ef47b47275209f4b.js
@@ -7,7 +7,7 @@
   \***********************************/
 /***/ ((module) => {
 
-module.exports = "AL AS AR CA CO CT DE FL GA HI ID IL IN IA LA NE NV NH NC ND OH PA RI SC SD TN TX UT VT VA WA WV WI\n";
+module.exports = "AL AR CA CO CT DE FL GA HI ID IL IN IA LA NE NV NH NC ND OH PA RI SC SD TN TX UT VT VA WA WV WI\n";
 
 /***/ })
 
