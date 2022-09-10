a4c3edefa004792badc410a36e02a117a46b0191
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-09-10 20:28:02 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@a3e686f0f7f9979c8e477763c2cf505a6ee690f2 🚀

---
+++ dev/src_wordfiles_INT3_json3c4e1463d72a39eae2ae.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_INT3_json"],{
+
+/***/ "./src/wordfiles/INT3.json":
+/*!*********************************!*\
+  !*** ./src/wordfiles/INT3.json ***!
+  \*********************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"letters":"abcdefghijklmnopqrstuvwxyz1234567890.,?/","minWordSize":3,"maxWordSize":3,"practiceSeconds":120}');
+
+/***/ })
+
+}]);
