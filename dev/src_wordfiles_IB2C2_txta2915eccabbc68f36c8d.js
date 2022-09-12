0ba4d253712afe83316a7ad8cb287be26e93868c
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-09-12 15:23:19 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@d22424e91c6b91af5f2d4f8970b4697216b0b170 🚀

---
+++ dev/src_wordfiles_IB2C2_txta2915eccabbc68f36c8d.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_IB2C2_txt"],{
+
+/***/ "./src/wordfiles/IB2C2.txt":
+/*!*********************************!*\
+  !*** ./src/wordfiles/IB2C2.txt ***!
+  \*********************************/
+/***/ ((module) => {
+
+module.exports = "<AR> <BT> <SK> 599 73 5NN ABT AGE AGN AM ANT B4 BEEN BK BUG C CALL CFM CLEAR CPI CPY CQ CU CUAGN CUL CW DE DN DR DX EL ES FB FER FM FT GA GE GM GN GND GUD HAM HI HPE HR HW HW? INFO K OK OM OP OT PSE PWR QRM QRN QRP QRQ QRS QRT QRZ QSB QSL QSO QSY QTH R RAIN RFI RIG RPRT RPT RR RST RX SIG SKED SOLID SRI SSB SUN T TEMP TKS TNX TU TX U UP UR VERT VY W WID WIND WPM WUD WX YAGI YRS\n";
+
+/***/ })
+
+}]);
