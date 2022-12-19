132b59d5bc56f44001e0f8eaab134b755b75f30b
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-12-19 14:03:24 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@f78b402f1e4686e15b438a8ebc5438bda439990b 🚀

---
+++ dev/src_wordfiles_SB2C4_txt46b711437f395f1811ab.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_SB2C4_txt"],{
+
+/***/ "./src/wordfiles/SB2C4.txt":
+/*!*********************************!*\
+  !*** ./src/wordfiles/SB2C4.txt ***!
+  \*********************************/
+/***/ ((module) => {
+
+module.exports = "W2LCW {DE|from} WB2UZE {FB|fine business} {CPY|copy} RICH {ES|and} {TNX|thanks} {FER|for} NICE {RPRT|report} \nRIG IC7300 {ES|and} {PWR|power} 100 {W|watts} \n{ANT|antenna} DIPOLE UP 35 {FT|feet} \t\n{WX|weather}  RAIN {ES|and} TEMP 40{F|fahrenheit}\nOK {HW?|how copy} {<AR>|end of message} W2LCW {DE|from} WB2UZE {K|invitation to transmit} \n";
+
+/***/ })
+
+}]);
