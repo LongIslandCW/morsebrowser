fbbf737861eec1ba8570b50c6766dab18f0e475c
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2022-12-19 14:54:47 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@bfc4dd8c4238112ff2b95b0fe141ab416d8cb6c6 🚀

--- dev/src_wordfiles_SB2C5_txtc8df3c9f3fe62bfbe719.js
+++ dev/src_wordfiles_SB2C5_txtc8df3c9f3fe62bfbe719.js
@@ -7,7 +7,7 @@
   \*********************************/
 /***/ ((module) => {
 
-module.exports = "W2LCW {DE|from} W2JSJ OK JOHN SOLID {CPY|copy} \nAGE 58 {YRS|years}\t\nBEEN HAM {FER|for} 30 {YRS|years}\nRETIRED TEACHER\nOK {HW?|how copy} {<AR>|end of message} W2LCW {DE|from} W2JSJ {K|invitation to transmit} \n";
+module.exports = "{W2LCW|w 2 l c w} {DE|from} {W2JSJ|w 2 j s j} OK JOHN SOLID {CPY|copy} \nAGE 58 {YRS|years}\t\nBEEN HAM {FER|for} 30 {YRS|years}\nRETIRED TEACHER\nOK {HW?|how copy} {<AR>|end of message} {W2LCW|w 2 l c w} {DE|from} {W2JSJ|w 2 j s j} {K|invitation to transmit} \n";
 
 /***/ })
 
