266c132667d450cce273426b062b0aeaeb47b853
Randy Loeb <githubrandyloeb@randonloeb.com>
2022-12-27 17:55:32 -0500

    fix flaggedwords

--- src/morse/flaggedWords/flaggedWords.ts
+++ src/morse/flaggedWords/flaggedWords.ts
@@ -1,4 +1,6 @@
 import * as ko from 'knockout'
+import MorseStringUtils from '../utils/morseStringUtils'
+import WordInfo from '../utils/wordInfo'
 export class FlaggedWords {
   flaggedWords:ko.Observable<string>
   flaggedWordsCount:ko.Computed<number>
@@ -11,7 +13,7 @@ export class FlaggedWords {
       if (!this.flaggedWords().trim()) {
         return 0
       }
-      return this.flaggedWords().trim().split(' ').length
+      return MorseStringUtils.getWords(this.flaggedWords(), false).length
     }, this)
   }
 
@@ -19,18 +21,21 @@ export class FlaggedWords {
     this.flaggedWords('')
   }
 
-  addFlaggedWord = (word) => {
+  addFlaggedWord = (word:WordInfo) => {
     if (!this.flaggedWords().trim()) {
-      this.flaggedWords(this.flaggedWords().trim() + word)
+      this.flaggedWords(this.flaggedWords().trim() + word.rawWord)
     } else {
       // deal with double click which is also used to pick a word
       const msNow = Date.now()
       const msPassedSince = msNow - this.lastFlaggedWordMs
       this.lastFlaggedWordMs = msNow
       const threshold = 500
-      const words = this.flaggedWords().trim().split(' ')
+
+      const words:WordInfo[] = this.flaggedWords() ? MorseStringUtils.getWords(this.flaggedWords(), false) : []
+
+      // const words = this.flaggedWords().trim().split(' ')
       const lastWord = words[words.length - 1]
-      if (lastWord === word && (msPassedSince < threshold)) {
+      if (lastWord.rawWord === word.rawWord && (msPassedSince < threshold)) {
         // we have a double click scenario so remove it
         words.pop()
       } else {
@@ -39,7 +44,7 @@ export class FlaggedWords {
       if (words.length === 0) {
         this.flaggedWords('')
       } else {
-        this.flaggedWords(words.join(' '))
+        this.flaggedWords(words.map(w => w.rawWord).join(' '))
       }
     }
   }
