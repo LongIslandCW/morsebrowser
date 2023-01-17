4fcfbb1335b2d683be41398847a4783eac87698e
RandyLoeb <34562250+RandyLoeb@users.noreply.github.com>
2023-01-17 03:47:53 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@3d33791cb142282488e9b53b1c10f425cdd0be28 🚀

---
+++ src_wordfiles_SB2C3_txt07f1262922195bb33d52.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_SB2C3_txt"],{
+
+/***/ "./src/wordfiles/SB2C3.txt":
+/*!*********************************!*\
+  !*** ./src/wordfiles/SB2C3.txt ***!
+  \*********************************/
+/***/ ((module) => {
+
+module.exports = "{W2LCW|w 2 l c w} {DE|from} {K2UPS|k 2 u p s} {GE|good evening} {ES|and} {TNX|thanks} {FER|for} CALL\t\n{UR|your} {RST|r s t} {599|five nine nine} {5NN|five nine nine} \n{QTH|location} LONG ISLAND, {NY|new york} LONG ISLAND, {NY|new york}\nNAME RICH RICH\nOK {HW?|how copy} {<AR>|end of message} {W2LCW|w 2 l c w} {DE|from} {K2UPS|k 2 u p s}\n";
+
+/***/ })
+
+}]);
---
+++ src_wordfiles_SB2C6_txt6a971d3a106738f196a5.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_SB2C6_txt"],{
+
+/***/ "./src/wordfiles/SB2C6.txt":
+/*!*********************************!*\
+  !*** ./src/wordfiles/SB2C6.txt ***!
+  \*********************************/
+/***/ ((module) => {
+
+module.exports = "{W2LCW|w 2 l c w} {DE|from} {WB0JRH|w b 0 j r h} OK RICH {TNX|thanks} {FER|for} {FB|fine business} {QSO|contact} \n{ES|and} {HP|hope} {CUAGN|see you again} 73 {<AR>|end of message} {W2LCW|w 2 l c w} {DE|from} {WB0JRH|w b 0 j r h} {TU|thank you} {<SK>|end of contact} \n";
+
+/***/ })
+
+}]);
