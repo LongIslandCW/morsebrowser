a12e67065ba19530119c9417be738468221afd54
Randy Loeb <githubrandyloeb@randonloeb.com>
2023-01-22 13:02:23 -0500

    settings key blacklisting

---
+++ src/morse/settings/settingsChangeInfo.ts
@@ -0,0 +1,24 @@
+import { MorseViewModel } from '../morse'
+import SavedSettingsInfo from './savedSettingsInfo'
+
+export class SettingsChangeInfo {
+  constructor (ctxt:MorseViewModel) {
+    this.ctxt = ctxt
+  }
+
+  ctxt:MorseViewModel
+
+  /* someday need to think through the contradictions */
+  ifLoadSettings:boolean
+
+  /* weather or not to ignore cookies */
+  ignoreCookies:boolean = false
+
+  /* settings to used. sort of conflicts with ifLoadSettings? */
+  custom:SavedSettingsInfo[] = null
+
+  /* don't change cookies, i.e. don't make this the user's preferred setting set */
+  lockoutCookieChanges:boolean = false
+
+  keyBlacklist:string[] = []
+}
