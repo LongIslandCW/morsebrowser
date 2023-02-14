ec2ae8702532882418e80a3a80788a1a8ebabd7a
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2023-02-14 15:28:22 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@a80643f6340a54badceaae67743c6d24bbd6f6a9 🚀

---
+++ dev/src_wordfiles_suduko1_txt7ef3024035f7b7044131.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_suduko1_txt"],{
+
+/***/ "./src/wordfiles/suduko1.txt":
+/*!***********************************!*\
+  !*** ./src/wordfiles/suduko1.txt ***!
+  \***********************************/
+/***/ ((module) => {
+
+module.exports = "3  8  5  1  4  6  7  2  9  0\n\n";
+
+/***/ })
+
+}]);
