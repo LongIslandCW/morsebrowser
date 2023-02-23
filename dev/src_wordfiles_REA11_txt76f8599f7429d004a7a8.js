c13942eff3bbd7e993ecb4fe2652c58ef2312b42
LongIslandCW <103384634+LongIslandCW@users.noreply.github.com>
2023-02-23 17:56:24 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@817655f06d7d779213d254c52493221f6eaca20a 🚀

--- dev/src_wordfiles_REA11_txt76f8599f7429d004a7a8.js
+++ dev/src_wordfiles_REA11_txt76f8599f7429d004a7a8.js
@@ -7,7 +7,7 @@
   \*********************************/
 /***/ ((module) => {
 
-module.exports = "RR FB\nHW R U\nHR ROB\nFB OB\nHW\nFER WHO\nUR HERE\nHW ARE U\nHEAR BOB\nWEAR ROBE\nBARE BEAR\nBREW BEER\nWORE FUR\nFEAR WAR\nOWE ROB\nEURO FARE\nHEAR HERB\nFOR WHO\nFAR WHARF\nEAR FUR\n";
+module.exports = "RR FB\nHW {R|r} U\nHR ROB\nFB OB\nHW\nFER WHO\nUR HERE\nHW ARE U\nHEAR BOB\nWEAR ROBE\nBARE BEAR\nBREW BEER\nWORE FUR\nFEAR WAR\nOWE ROB\nEURO FARE\nHEAR HERB\nFOR WHO\nFAR WHARF\nEAR FUR\n";
 
 /***/ })
 
