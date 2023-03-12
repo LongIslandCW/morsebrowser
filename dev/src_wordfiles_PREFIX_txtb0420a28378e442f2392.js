620037bc9f08125d904aa2149d9ff8a5f2604125
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2023-03-12 15:15:14 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@d8d93a139ece3fd340548006b7cd3cbceec12424 🚀

---
+++ dev/src_wordfiles_PREFIX_txtb0420a28378e442f2392.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_PREFIX_txt"],{
+
+/***/ "./src/wordfiles/PREFIX.txt":
+/*!**********************************!*\
+  !*** ./src/wordfiles/PREFIX.txt ***!
+  \**********************************/
+/***/ ((module) => {
+
+module.exports = "PRO  PRODUCTS PRODUCT PROGRAM \r\nCON - CONTACT, CONTROL, CONTENT \r\nCOM - COMPANY COMMENTS COMMUNITY";
+
+/***/ })
+
+}]);
---
+++ dev/src_wordfiles_SUFFIX_txtfd6b5fcc9876736dc601.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_SUFFIX_txt"],{
+
+/***/ "./src/wordfiles/SUFFIX.txt":
+/*!**********************************!*\
+  !*** ./src/wordfiles/SUFFIX.txt ***!
+  \**********************************/
+/***/ ((module) => {
+
+module.exports = "ING USING RATING MAKING \r\nION SECTION ACTION EDUCATION \r\nENT RECENT DEPARTMENT PERCENT \r\n\r\n";
+
+/***/ })
+
+}]);
