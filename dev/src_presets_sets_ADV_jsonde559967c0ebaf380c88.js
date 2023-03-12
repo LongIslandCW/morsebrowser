97ab746b7c9f5d14ceca846e1449bb8c828f4ce3
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2023-03-12 21:20:46 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@ea8267a412a1b61af1565515dd613d3b95090a33 🚀

---
+++ dev/src_presets_sets_ADV_jsonde559967c0ebaf380c88.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_presets_sets_ADV_json"],{
+
+/***/ "./src/presets/sets/ADV.json":
+/*!***********************************!*\
+  !*** ./src/presets/sets/ADV.json ***!
+  \***********************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"options":[{"display":"Default 25/25","filename":"ADVBUF.json"},{"display":"Sentences","filename":"ADVBUF.json"},{"display":"2 Words","filename":"ADV2WB.json"},{"display":"3 Words","filename":"ADV3WB.json"},{"display":"4 Words","filename":"ADV4WB.json"},{"display":"5 Words","filename":"ADV5WB.json"},{"display":"Prefixes","filename":"ADVBUF.json"},{"display":"Suffixes","filename":"ADVBUF.json"},{"display":"2 Words","filename":"ADV2WB.json"},{"display":"3 Words","filename":"ADV3WB.json"},{"display":"4 Words","filename":"ADV4WB.json"},{"display":"5 Words","filename":"ADV5WB.json"}]}');
+
+/***/ })
+
+}]);
