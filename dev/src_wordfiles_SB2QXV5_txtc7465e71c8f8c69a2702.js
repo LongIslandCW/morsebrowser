2388dc0fb65b0f95470d17d03a5b42a7e0eee2d3
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2023-03-18 19:16:45 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@ea5eaea7d7e45ee33cafc9d4a84a63d319c060cd 🚀

--- dev/src_wordfiles_SB2QXV5_txtc7465e71c8f8c69a2702.js
+++ dev/src_wordfiles_SB2QXV5_txtc7465e71c8f8c69a2702.js
@@ -7,7 +7,7 @@
   \***********************************/
 /***/ ((module) => {
 
-module.exports = "AL \nAR \nCA \nCO \nCT \n{DE|delaware} \nFL \n{GA|georgia} \n{HI|hawaii} \n{ID|idaho} \nIL \n{IN|indiana} \nIA \n{LA|louisiana} \nNE \nNV \nNH \nNC \nND \n{OH|ohio} \nPA \nRI \nSC \nSD \nTN \nTX \nUT \nVT \nVA \nWA \nWV \nWI \n";
+module.exports = "AL \nAR \nCA \nCO \nCT \n{DE|delaware} \nFL \n{GA|georgia} \n{HI|hawaii} \n{ID|idaho} \nIL \n{IN|indiana} \nIA \n{LA|louisiana} \nNE \nNV \nNH \nNC \nND \n{OH|ohio} \n{OR|oregon} \nPA \nRI \nSC \nSD \nTN \nTX \nUT \nVT \nVA \nWA \nWV \nWI \n";
 
 /***/ })
 
