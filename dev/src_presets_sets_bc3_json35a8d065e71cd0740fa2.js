555f6597398229816d0714d12429e037302bffc2
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2023-04-04 21:13:00 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@588363713aa343aacb3a1e7f71d7c7c2ae48c84a 🚀

---
+++ dev/src_presets_sets_bc3_json35a8d065e71cd0740fa2.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_presets_sets_bc3_json"],{
+
+/***/ "./src/presets/sets/bc3.json":
+/*!***********************************!*\
+  !*** ./src/presets/sets/bc3.json ***!
+  \***********************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"options":[{"display":"Default 12/12","filename":"BPT_Default_12.json"},{"display":"Default 15/12","filename":"BPT_Default_15.json"},{"display":"Call Signs","filename":"bc3_cs.json"},{"display":"Sending Practice","filename":"BPT_Sending.json"},{"display":"Voice On & Spell On","filename":"BPT_Von_Son.json"},{"display":"Voice On & Spell Off","filename":"BPT_Von_Soff.json"}]}');
+
+/***/ })
+
+}]);
