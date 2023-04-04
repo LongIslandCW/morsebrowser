3c900dee94e6d77d7ff080c8cce14c8804e367bf
Randy Loeb <githubrandyloeb@randonloeb.com>
2023-04-04 19:10:08 -0400

    sync

--- src/morse/morsePresetSetFinder.js
+++ src/morse/morsePresetSetFinder.js
@@ -15,14 +15,17 @@ export class MorsePresetSetFileFinder {
       case 'bc2.json':
         import('../presets/sets/bc2.json').then(({ default: x }) => afterFound({ found: true, data: x }))
         break
-      case 'bpt.json':
-        import('../presets/sets/bpt.json').then(({ default: x }) => afterFound({ found: true, data: x }))
+      case 'bc3.json':
+        import('../presets/sets/bc3.json').then(({ default: x }) => afterFound({ found: true, data: x }))
         break
       case 'int_12_16.json':
         import('../presets/sets/int_12_16.json').then(({ default: x }) => afterFound({ found: true, data: x }))
         break
       case 'int_16_20.json':
         import('../presets/sets/int_16_20.json').then(({ default: x }) => afterFound({ found: true, data: x }))
+        break
+      case 'int_1_12.json':
+        import('../presets/sets/int_1_12.json').then(({ default: x }) => afterFound({ found: true, data: x }))
         break
         // BEGINB
       default:
