746e97626d7e42f984c54634d4ed19268feead2d
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2023-04-12 17:52:12 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@9db7ef1ae3bf750cd0723009c42da86dd07dc0e4 🚀

---
+++ dev/src_wordfiles_B2_PRO_1W_txtd30e362d0057b58098a0.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_B2_PRO_1W_txt"],{
+
+/***/ "./src/wordfiles/B2_PRO_1W.txt":
+/*!*************************************!*\
+  !*** ./src/wordfiles/B2_PRO_1W.txt ***!
+  \*************************************/
+/***/ ((module) => {
+
+module.exports = "CPI <BT>\r\nSRI  <SK>\r\nBTU <AR>\r\nHW <BT>\r\nSOLID <AR>\r\nCLEAR <SK>\r\nAGN <BT>\r\nCUAGN <SK>\r\nPWR <BT>\r\nSIG <BT>\r\nRAIN <AR>\r\nRIG <BT>\r\nCALL <AR>\r\nRETIRED <BT>\r\nWUD <SK>\r\nFER <AR>\r\nBEEN <BT>\r\nES <AR>\r\nRST <SK>\r\nPSE <BT>\r\nINFO <AR>";
+
+/***/ })
+
+}]);
