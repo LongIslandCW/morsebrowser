5582b8927c5647974966430ae985a699ce1de7b3
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2023-05-11 16:03:15 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@328cba2c5e4653433e67b94a531283c0dc3c81c9 🚀

---
+++ dev/src_wordfiles_BC2_P_Eval_Sent_txt9ff2a9f57e100e4dfade.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_BC2_P_Eval_Sent_txt"],{
+
+/***/ "./src/wordfiles/BC2_P_Eval_Sent.txt":
+/*!*******************************************!*\
+  !*** ./src/wordfiles/BC2_P_Eval_Sent.txt ***!
+  \*******************************************/
+/***/ ((module) => {
+
+module.exports = "JACK AMAZED A FEW GIRLS BY DROPPING THE ANTIQUE ONYX VASE. \r\nWE PROMPTLY JUDGED ANTIQUE IVORY BUCKLES FOR THE NEXT PRIZE. \r\nJINXED WIZARDS PLUCK IVY FROM THE BIG QUILT. \r\nCRAZY FREDERICKA BOUGHT MANY VERY EXQUISITE OPAL JEWELS. \r\nTHE EXODUS OF JAZZY PIGEONS IS CRAVED BY SQUEAMISH WALKERS. \r\nWHENEVER THE BLACK FOX JUMPED, THE SQUIRREL GAZED SUSPICIOUSLY. \r\nTHE HUGE SPHINX OF BLACK QUARTZ, JUDGE MY VOW NOW. \r\nHOW QUICKLY CAN THE DAFT JUMPING ZEBRAS VEX ME? \r\nHEAVY WHITE BOXES PERFORM QUICK WALTZES AND JIGS FOR ME. ";
+
+/***/ })
+
+}]);
