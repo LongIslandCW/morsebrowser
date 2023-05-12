fd78c8d05e7478b3917773bfe0b416887eefee03
RandyLoeb <34562250+RandyLoeb@users.noreply.github.com>
2023-05-12 02:44:22 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@20057689e2c57f63c8bf015ab0cfd7dc76f7fc4b 🚀

---
+++ src_wordfiles_BC2_P-Eval_CS_txt1fdc024ea60804e67824.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_BC2_P-Eval_CS_txt"],{
+
+/***/ "./src/wordfiles/BC2_P-Eval_CS.txt":
+/*!*****************************************!*\
+  !*** ./src/wordfiles/BC2_P-Eval_CS.txt ***!
+  \*****************************************/
+/***/ ((module) => {
+
+module.exports = "WO6ZQ AF2JCW NB4RU NS8EY KØDL NV1HG W9PAM AX5TI N3GE N7RD \r\nN1SM W6ZF W2RT NØAB AC7LD NA3ILG KB9XO WP4J WY5HQ N8VEU \r\nN5DS W1AW VE8TO W2LC AC3Y WBØHQ AC6PFAJ7TIZ WX9ER KG4UM \r\nVK1TF W6ARF K2NDG AB8U WØIS  AP4NQ W5HOY N9CDL N7XZ WM3EJ \r\nAH7CUL N3IE AI4PL GØPXT WB9R AA5TIY K2DG WJ8FQ WO6Z VE1SM ";
+
+/***/ })
+
+}]);
