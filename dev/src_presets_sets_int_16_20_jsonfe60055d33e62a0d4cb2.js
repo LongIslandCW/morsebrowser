6de00802e19e981c94fba880f204cf89bdf86a76
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2023-05-22 19:49:56 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@7fc7cf91619e06d1b6198f83cad8d21b4ca28aae 🚀

---
+++ dev/src_presets_sets_int_16_20_jsonfe60055d33e62a0d4cb2.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_presets_sets_int_16_20_json"],{
+
+/***/ "./src/presets/sets/int_16_20.json":
+/*!*****************************************!*\
+  !*** ./src/presets/sets/int_16_20.json ***!
+  \*****************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"options":[{"display":"Default 16/16","filename":"INT2_Default_16.json"},{"display":"Default 20/20","filename":"INT2_Default_20.json"},{"display":"Random Groups 1-5","filename":"INT2_Random_1_5.json"},{"display":"VST","filename":"INT2_VST.json"},{"display":"Reverse VST","filename":"INT2_RVST.json"},{"display":"Groups of 5","filename":"INT2_Groups_5.json"},{"display":"Inter-character Head Copy","filename":"INT2_ICHC.json"},{"display":"ICR","filename":"INT2_ICR.json"},{"display":"VET","filename":"INT2_VET.json"},{"display":"Reverse VET","filename":"INT2_RVET.json"},{"display":"Instant Word Recognition","filename":"IWR.json"},{"display":"Sending Practice","filename":"INT2_Sending.json"},{"display":"Progressive Word Building","filename":"INT2_PWG.json"},{"display":"Progressive Phrase Building","filename":"INT_3_PPB.json"},{"display":"Suduko","filename":"suduko20.json"},{"display":"Voice On & Spell On","filename":"INT2_Von_Son.json"},{"display":"Voice On & Spell Off","filename":"INT2_Von_Soff.json"}]}');
+
+/***/ })
+
+}]);
