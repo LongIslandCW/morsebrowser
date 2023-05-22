ed792178de27708c3298bd45fdf039dda2a41f1e
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2023-05-22 20:31:18 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@85e93c715cf0c99346d0fe906bdb2e06816bb544 🚀

---
+++ dev/src_wordfiles_temp_txtec2c640a1a07c081a0fd.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_temp_txt"],{
+
+/***/ "./src/wordfiles/temp.txt":
+/*!********************************!*\
+  !*** ./src/wordfiles/temp.txt ***!
+  \********************************/
+/***/ ((module) => {
+
+module.exports = "TKS \r\n{TKS|} \r\n{TKS|} \r\n{TKS|} \r\n{TKS|} \r\n{TKS|} \r\n{TKS|} \r\n{|} ";
+
+/***/ })
+
+}]);
