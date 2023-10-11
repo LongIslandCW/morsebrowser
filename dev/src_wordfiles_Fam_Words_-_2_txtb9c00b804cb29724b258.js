4aa33890ac952d794bd9c434243eaa5c875ab33c
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2023-10-11 20:57:11 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@62d9eba87a99617ed7d4fde319e084b936e4aee0 🚀

---
+++ dev/src_wordfiles_Fam_Words_-_2_txtb9c00b804cb29724b258.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_Fam_Words_-_2_txt"],{
+
+/***/ "./src/wordfiles/Fam_Words - 2.txt":
+/*!*****************************************!*\
+  !*** ./src/wordfiles/Fam_Words - 2.txt ***!
+  \*****************************************/
+/***/ ((module) => {
+
+module.exports = "he  \n{he|} \n{he|} \n{he|} \nhe  \n{|} \r\nhow  \n{how|} \n{how|} \n{how|} \nhow  \n{|} \r\nby  \n{by|} \n{by|} \n{by|} \nby  \n{|} \r\ncould  \n{could|} \n{could|} \n{could|} \ncould  \n{|} \r\nhis  \n{his|} \n{his|} \n{his|} \nhis  \n{|} \r\nnot  \n{not|} \n{not|} \n{not|} \nnot  \n{|} \r\nno  \n{no|} \n{no|} \n{no|} \nno  \n{|} \r\nperson  \n{person|} \n{person|} \n{person|} \nperson  \n{|} \r\non  \n{on|} \n{on|} \n{on|} \non  \n{|} \r\nwork  \n{work|} \n{work|} \n{work|} \nwork  \n{|} \r\ngood  \n{good|} \n{good|} \n{good|} \ngood  \n{|} \r\nknow  \n{know|} \n{know|} \n{know|} \nknow  \n{|} \r\nwhen  \n{when|} \n{when|} \n{when|} \nwhen  \n{|} \r\nI  \n{I|} \n{I|} \n{I|} \nI  \n{|} \r\nlook  \n{look|} \n{look|} \n{look|} \nlook  \n{|} \r\ngo  \n{go|} \n{go|} \n{go|} \ngo  \n{|} \r\nday  \n{day|} \n{day|} \n{day|} \nday  \n{|} \r\ncome  \n{come|} \n{come|} \n{come|} \ncome  \n{|} \r\nover  \n{over|} \n{over|} \n{over|} \nover  \n{|} \r\nsay  \n{say|} \n{say|} \n{say|} \nsay  \n{|} \r\nto  \n{to|} \n{to|} \n{to|} \nto  \n{|} \r\nfor  \n{for|} \n{for|} \n{for|} \nfor  \n{|} \r\nabout  \n{about|} \n{about|} \n{about|} \nabout  \n{|} \r\nthem  \n{them|} \n{them|} \n{them|} \nthem  \n{|} \r\ncan  \n{can|} \n{can|} \n{can|} \ncan  \n{|} \r\n";
+
+/***/ })
+
+}]);
---
+++ dev/src_wordfiles_Fam_Words_-_3_txt390ad288c50e52d9247f.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_Fam_Words_-_3_txt"],{
+
+/***/ "./src/wordfiles/Fam_Words - 3.txt":
+/*!*****************************************!*\
+  !*** ./src/wordfiles/Fam_Words - 3.txt ***!
+  \*****************************************/
+/***/ ((module) => {
+
+module.exports = "way  \n{way|} \n{way|} \n{way|} \nway  \n{|} \r\nthere  \n{there|} \n{there|} \n{there|} \nthere  \n{|} \r\nfrom  \n{from|} \n{from|} \n{from|} \nfrom  \n{|} \r\nour  \n{our|} \n{our|} \n{our|} \nour  \n{|} \r\nmy  \n{my|} \n{my|} \n{my|} \nmy  \n{|} \r\nsee  \n{see|} \n{see|} \n{see|} \nsee  \n{|} \r\nany  \n{any|} \n{any|} \n{any|} \nany  \n{|} \r\nbe  \n{be|} \n{be|} \n{be|} \nbe  \n{|} \r\nget  \n{get|} \n{get|} \n{get|} \nget  \n{|} \r\nas  \n{as|} \n{as|} \n{as|} \nas  \n{|} \r\nit  \n{it|} \n{it|} \n{it|} \nit  \n{|} \r\nonly  \n{only|} \n{only|} \n{only|} \nonly  \n{|} \r\nfirst  \n{first|} \n{first|} \n{first|} \nfirst  \n{|} \r\nif  \n{if|} \n{if|} \n{if|} \nif  \n{|} \r\nat  \n{at|} \n{at|} \n{at|} \nat  \n{|} \r\na  \n{a|} \n{a|} \n{a|} \na  \n{|} \r\nthe  \n{the|} \n{the|} \n{the|} \nthe  \n{|} \r\nyou  \n{you|} \n{you|} \n{you|} \nyou  \n{|} \r\nout  \n{out|} \n{out|} \n{out|} \nout  \n{|} \r\nwill  \n{will|} \n{will|} \n{will|} \nwill  \n{|} \r\nwith  \n{with|} \n{with|} \n{with|} \nwith  \n{|} \r\nher  \n{her|} \n{her|} \n{her|} \nher  \n{|} \r\nwe  \n{we|} \n{we|} \n{we|} \nwe  \n{|} \r\nthink  \n{think|} \n{think|} \n{think|} \nthink  \n{|} \r\ntime  \n{time|} \n{time|} \n{time|} \ntime  \n{|} \r\n";
+
+/***/ })
+
+}]);
