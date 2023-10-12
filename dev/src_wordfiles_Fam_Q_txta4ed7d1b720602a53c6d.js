56c9d80feb2e5967b6c91d166ac14052cb2e21fb
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2023-10-12 21:01:52 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@e218755863b9c8aa4d1dd43077c3af110961a3ef 🚀

---
+++ dev/src_wordfiles_Fam_Q_txta4ed7d1b720602a53c6d.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_Fam_Q_txt"],{
+
+/***/ "./src/wordfiles/Fam_Q.txt":
+/*!*********************************!*\
+  !*** ./src/wordfiles/Fam_Q.txt ***!
+  \*********************************/
+/***/ ((module) => {
+
+module.exports = "{QTH|QTH } \n{QTH|} \n{QTH|} \n{|} \r\n{QRL|QRL}  \n{QRL|} \n{QRL|} \n{|} \r\n{QRM|QRM} \n{QRM|} \n{QRM|} \n{|} \r\n{QRN|QRN} \n{QRN|} \n{QRN|} \n{|} \r\n{QRP|QRP} \n{QRP|} \n{QRP|} \n{|} \r\n{QRQ|QRQ} \n{QRQ|} \n{QRQ|} \n{|} \r\n{QRS|QRS} \n{QRS|} \n{QRS|} \n{|} \r\n{QRT|QRT} \n{QRT|} \n{QRT|} \n{|} \r\n{QRZ|Q R Z} \n{QRZ|} \n{QRZ|} \n{|} \r\n{QSB|QSB} \n{QSB|} \n{QSB|} \n{|} \r\n{QSY|QSY} \n{QSY|} \n{QSY|} \n{|} \r\n{QTH|QTH } \n{QTH|} \n{QTH|} \n{|} \r\n{QRL|QRL}  \n{QRL|} \n{QRL|} \n{|} \r\n{QRM|QRM} \n{QRM|} \n{QRM|} \n{|} \r\n{QRN|QRN} \n{QRN|} \n{QRN|} \n{|} \r\n{QRP|QRP} \n{QRP|} \n{QRP|} \n{|} \r\n{QRQ|QRQ} \n{QRQ|} \n{QRQ|} \n{|} \r\n{QRS|QRS} \n{QRS|} \n{QRS|} \n{|} \r\n{QRT|QRT} \n{QRT|} \n{QRT|} \n{|} \r\n{QRZ|Q R Z} \n{QRZ|} \n{QRZ|} \n{|} \r\n{QSB|QSB} \n{QSB|} \n{QSB|} \n{|} \r\n{QSY|QSY} \n{QSY|} \n{QSY|} \n{|} \r\n";
+
+/***/ })
+
+}]);
