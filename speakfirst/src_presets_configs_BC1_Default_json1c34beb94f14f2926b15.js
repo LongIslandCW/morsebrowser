76d6d2144354a7831e153b90d2ed9401dd06aa6b
RandyLoeb <34562250+RandyLoeb@users.noreply.github.com>
2023-12-21 02:31:10 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@3a79da5bb72fd35b14adf8b301e12afb5b6c2556 🚀

---
+++ speakfirst/src_presets_configs_BC1_Default_json1c34beb94f14f2926b15.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_presets_configs_BC1_Default_json"],{
+
+/***/ "./src/presets/configs/BC1_Default.json":
+/*!**********************************************!*\
+  !*** ./src/presets/configs/BC1_Default.json ***!
+  \**********************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"morseSettings":[{"key":"wpm","value":12,"comment":null},{"key":"fwpm","value":8,"comment":null},{"key":"preSpace","value":"0","comment":null},{"key":"xtraWordSpaceDits","value":"1","comment":null},{"key":"volume","value":"7","comment":null},{"key":"stickySets","value":"BK","comment":null},{"key":"ifStickySets","value":false,"comment":null},{"key":"syncWpm","value":false,"comment":null},{"key":"hideList","value":true,"comment":null},{"key":"showRaw","value":false,"comment":null},{"key":"autoCloseLessonAccordian","value":false,"comment":null},{"key":"cardFontPx","value":"15","comment":null},{"key":"customGroup","value":"","comment":null},{"key":"showExpertSettings","value":"true","comment":null},{"key":"voiceEnabled","value":true,"comment":null},{"key":"voiceSpelling","value":true,"comment":null},{"key":"voiceThinkingTime","value":0,"comment":null},{"key":"voiceAfterThinkingTime","value":0,"comment":null},{"key":"voiceVolume","value":10,"comment":null},{"key":"voiceLastOnly","value":false,"comment":null},{"key":"voiceRecap","value":true,"comment":null},{"key":"keepLines","value":false,"comment":null},{"key":"syncSize","value":true,"comment":null},{"key":"overrideSize","value":false,"comment":null},{"key":"overrideSizeMin","value":3,"comment":null},{"key":"overrideSizeMax","value":3,"comment":null},{"key":"cardSpace","value":0,"comment":"AKA cardWait"},{"key":"hapticAccordionOpen","value":"false","comment":null},{"key":"miscSettingsAccordionOpen","value":"false","comment":null},{"key":"speedInterval","value":false,"comment":null},{"key":"intervalTimingsText","value":"","comment":null},{"key":"intervalWpmText","value":"","comment":null},{"key":"intervalFwpmText","value":"","comment":null},{"key":"voiceBufferMaxLength","value":"1","comment":null},{"key":"speakFirst","value":false,"comment":null},{"key":"speakFirstRepeats","value":0,"comment":null},{"key":"speakFirstAdditionalWordspaces","value":0,"comment":null}]}');
+
+/***/ })
+
+}]);
