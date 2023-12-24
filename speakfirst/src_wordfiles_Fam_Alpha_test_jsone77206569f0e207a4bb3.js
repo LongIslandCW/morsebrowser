7587c98143cb03a104a1baed5cdc2aa733086446
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2023-12-24 14:59:09 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@d947ab6b06eeafab3580fa57ce6ef8dc0b1b925e 🚀

---
+++ speakfirst/src_wordfiles_Fam_Alpha_test_jsone77206569f0e207a4bb3.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_Fam_Alpha_test_json"],{
+
+/***/ "./src/wordfiles/Fam_Alpha test.json":
+/*!*******************************************!*\
+  !*** ./src/wordfiles/Fam_Alpha test.json ***!
+  \*******************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"letters":"abcdefghijklmnopqrstuvwxyz","minWordSize":1,"maxWordSize":1,"practiceSeconds":120}');
+
+/***/ })
+
+}]);
