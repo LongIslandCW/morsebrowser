7ab4ae4120b898bf19ab49e41a4b510606ff85b5
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2023-12-27 17:53:52 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@11b209ee219d46611518908dae83582e80eaa8b7 🚀

---
+++ speakfirst/src_presets_sets_ADV_json52d263f865a859d6506c.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_presets_sets_ADV_json"],{
+
+/***/ "./src/presets/sets/ADV.json":
+/*!***********************************!*\
+  !*** ./src/presets/sets/ADV.json ***!
+  \***********************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"options":[{"display":"Default 25/25","filename":"ADVBUF.json"},{"display":"ICR","filename":"ADVICR.json"},{"display":"Sentences","filename":"ADVBUF.json"},{"display":"Instant Word Recognition","filename":"IWR.json"},{"display":"Voice after 2","filename":"ADV2WB.json"},{"display":"Voice after 3","filename":"ADV3WB.json"},{"display":"Voice after 4","filename":"ADV4WB.json"},{"display":"Voice after 5","filename":"ADV5WB.json"},{"display":"Prefixes","filename":"ADVBUF.json"},{"display":"Suffixes","filename":"ADVBUF.json"},{"display":"Familiarity 25wpm","filename":"ADV_FAM_Spell_25.json"},{"display":"Familiarity 30wpm","filename":"ADV_FAM_Spell_30.json"},{"display":"Familiarity 35wpm","filename":"ADV_FAM_Spell_35.json"}]}');
+
+/***/ })
+
+}]);
