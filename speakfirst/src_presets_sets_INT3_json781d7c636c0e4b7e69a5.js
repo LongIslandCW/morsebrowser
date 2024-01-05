b2af15f0f3cb162c810384789bd148ba5ae31b2c
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2024-01-05 01:56:54 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@dcde9f69bb92227d9fd2b3b155e6b0a399bac95c 🚀

---
+++ speakfirst/src_presets_sets_INT3_json781d7c636c0e4b7e69a5.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_presets_sets_INT3_json"],{
+
+/***/ "./src/presets/sets/INT3.json":
+/*!************************************!*\
+  !*** ./src/presets/sets/INT3.json ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"options":[{"display":"Default 16/16","filename":"INT2_Default_16.json"},{"display":"Default 20/20","filename":"INT2_Default_20.json"},{"display":"Familiarity Phrases","filename":"INT3_FAM_Phrases.json"},{"display":"Familiarity Spelling","filename":"INT3_FAM_Spell.json"},{"display":"Familiarity Words","filename":"INT3_FAM_Words.json"},{"display":"Groups of 5","filename":"INT2_Groups_5.json"},{"display":"ICR","filename":"INT2_ICR.json"},{"display":"Phrases","filename":"INT2_Phrases.json"},{"display":"Random Groups 1-5","filename":"INT2_Random_1_5.json"},{"display":"Reverse VET","filename":"INT2_RVET.json"},{"display":"Reverse VST","filename":"INT2_RVST.json"},{"display":"Sending Practice","filename":"INT2_Sending.json"},{"display":"Suduko","filename":"suduko20.json"},{"display":"VET","filename":"INT2_VET.json"},{"display":"VST","filename":"INT2_VST.json"},{"display":"Voice On & Spell On","filename":"INT2_Von_Son.json"},{"display":"Voice On & Spell Off","filename":"INT2_Von_Soff.json"}]}');
+
+/***/ })
+
+}]);
