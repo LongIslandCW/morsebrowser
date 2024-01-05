7786e09d28272e1331e48160d2b3060b60ba7faa
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2024-01-05 01:26:24 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@4affc6172404f4817afa4b859ecdf170327e8854 🚀

---
+++ speakfirst/src_presets_sets_ADV3_json9d568c43076a04cead62.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_presets_sets_ADV3_json"],{
+
+/***/ "./src/presets/sets/ADV3.json":
+/*!************************************!*\
+  !*** ./src/presets/sets/ADV3.json ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"options":[{"display":"Default 35/35","filename":"ADV3BUF.json"},{"display":"Familiarity Spell","filename":"ADV3_FAM_Spell.json"},{"display":"Familiarity Words","filename":"ADV3_FAM_WORDS.json"},{"display":"Familiarity Phrases","filename":"ADV3_FAM_Phrases.json"},{"display":"ICR","filename":"ADVICR.json"},{"display":"Prefixes","filename":"ADVBUF.json"},{"display":"Sentences","filename":"ADVBUF.json"},{"display":"Suffixes","filename":"ADVBUF.json"},{"display":"Voice after 2","filename":"ADV2WB.json"}]}');
+
+/***/ })
+
+}]);
