8bb548cf5bb6f04b323b82fd15fa457c3aae365d
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2024-01-07 22:05:06 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@6a671ac1f8112cfc41b1620c12f98810e1b3eca2 🚀

---
+++ speakfirst/src_wordfiles_INT1_FAM_Sentences_3W_txt30693b74d1f6b9e615c2.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_INT1_FAM_Sentences_3W_txt"],{
+
+/***/ "./src/wordfiles/INT1_FAM_Sentences_3W.txt":
+/*!*************************************************!*\
+  !*** ./src/wordfiles/INT1_FAM_Sentences_3W.txt ***!
+  \*************************************************/
+/***/ ((module) => {
+
+module.exports = "Ill be there.\r\nI love you.\r\nMaybe you�re right.\r\nI trust you.\r\nGo for it.\r\nGot your back.\r\nHow are you?\r\nI want you.\r\nI respect you.\r\nPlease forgive me.\r\nNow or never.\r\nGet enough sleep.\r\nI miss you.\r\nNurture your best.\r\nLets just dance.\r\nLet it go.\r\nTry something new.\r\nKeep it legal.\r\nI am sorry.\r\nThanks so much.\r\nProtect your health.\r\nDo it now.\r\nAppreciate the moment.\r\nBe a giver.\r\nChange is good.\r\nCount your blessings.\r\nAgainst all odds.\r\nAll is well.\r\nTry something new.\r\nDreams come true.\r\nNever give up.\r\nWinners never quit.\r\nSuccess breeds success.\r\nNever look back.\r\nNow or never.\r\nMake it happen.\r\nGood vibes only.\r\nFeed your soul.\r\nYou are enough.\r\nNobody is perfect.\r\nKeep it cool.\r\nLearn from yesterday.\r\nTry something new.\r\nNever stop dreaming.\r\nLearn from yesterday.\r\nKeep it fun.\r\nDon�t drive drunk.\r\nCelebrate your victories.\r\nLets be friends.\r\nThis is music.\r\nOver and out.\r\nWhere are you?\r\nWake your dreams.\r\nLife won�t wait.\r\nBelieve in yourself.\r\nLet it be.\r\nHold my hand.\r\nWho are you?\r\nThis will pass.\r\nSpeak the truth.\r\nLive your potential.\r\nLove conquers all.\r\nLove endures delay.\r\nLove your job.\r\nLove your parents.\r\nMaintain your integrity.\r\nMake enough money.\r\nHave meaningful goals.\r\nMake new friends.\r\nMake people grin.\r\nMake somebody�s day.\r\nMake things happy.\r\nNever hold grudges.\r\nNever look back.\r\nNo strings attached.\r\nNurture your best.\r\nOrganize your life.\r\nPick yourself flowers.\r\nPlan your vacation.\r\nPlease forgive me.\r\nRain will fall.\r\nRead a book.\r\nRead interesting articles.\r\nRemember to live.\r\nRespect your elders.\r\nSave every penny.\r\nSeize the day.\r\nShine your light.\r\nCreativity takes courage.\r\nDon�t overthink it.\r\nTry new things.\r\nNourish your soul.\r\nBe the exception.\r\nConquer from within.\r\nEscape the ordinary.\r\n\r\n";
+
+/***/ })
+
+}]);
