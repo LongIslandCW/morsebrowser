c35964649962e31d512e6a7938f28b733e348ce6
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2024-01-10 17:58:43 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@1cd30e94da358d002d5941ab2c1eb8eeaea29585 🚀

---
+++ dev/src_wordfiles_BC3_Phrases_Field_Day_txtb98c8e9cdcf81a163508.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_BC3_Phrases_Field_Day_txt"],{
+
+/***/ "./src/wordfiles/BC3_Phrases_Field_Day.txt":
+/*!*************************************************!*\
+  !*** ./src/wordfiles/BC3_Phrases_Field_Day.txt ***!
+  \*************************************************/
+/***/ ((module) => {
+
+module.exports = "WC3L TU 4F {OH|ohio} \r\n{<BT>|} \r\nK3VB TU 2E {WMA|westerm Massaachusetts}\r\n{<BT>|} \r\nKT5WZL TU 2E {WTX|west texas} \r\n{<BT>|} \r\nN9RXF TU 1A IA \r\n{<BT>|} \r\nWO2X TU 2E MS \r\n{<BT>|} \r\nK0GNB TU 10A MO \r\n{<BT>|} \r\nKK2X TU 2B {GA|georga} \r\n{<BT>|} \r\n{A7SH|A 7 S H} TU 4C KS \r\n{<BT>|} \r\n{A6TO|A 6 T O } TU 1E UT \r\n{<BT>|} \r\nK3SR TU 1A {ORG|orange county} ";
+
+/***/ })
+
+}]);
