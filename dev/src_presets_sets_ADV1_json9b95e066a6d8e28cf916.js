d4057a4e2a6ec3de24cb95efa4b555e7188003d8
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2024-01-20 01:33:38 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@4e7902e030f9d1991f913e304ee4d5c6e46dfd50 🚀

---
+++ dev/src_presets_sets_ADV1_json9b95e066a6d8e28cf916.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_presets_sets_ADV1_json"],{
+
+/***/ "./src/presets/sets/ADV1.json":
+/*!************************************!*\
+  !*** ./src/presets/sets/ADV1.json ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"options":[{"display":"Default 25/25","filename":"ADV1BUF.json"},{"display":"Affixes","filename":"ADV1BUF.json"},{"display":"Familiarity Phrases","filename":"ADV1_FAM_Phrases.json"},{"display":"Familiarity Sentences","filename":"ADV1_FAM_Sentences.json"},{"display":"Familiarity Spell","filename":"ADV1_FAM_Spell.json"},{"display":"Familiarity Words","filename":"ADV1_FAM_WORDS.json"},{"display":"ICR","filename":"ADVICR.json"},{"display":"Recognition","filename":"ADV1_Recognition.json"}]}');
+
+/***/ })
+
+}]);
