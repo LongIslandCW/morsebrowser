f25d5a17e2bd79f67f09abaffba1a944d022d43c
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2024-01-20 21:40:27 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@9bbb5d67ad0125ea47de1c2e1b2d1e84ff1cb701 🚀

---
+++ dev/src_wordfiles_Words_5L_txt905fcd843395ae8074d3.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_Words_5L_txt"],{
+
+/***/ "./src/wordfiles/Words_5L.txt":
+/*!************************************!*\
+  !*** ./src/wordfiles/Words_5L.txt ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = "stick \r\nridge \r\ncable \r\nfault \r\nclean \r\nallow \r\nankle \r\nquiet \r\nstock \r\nchart \r\ntough \r\ncoast \r\ntrend \r\ncross \r\ngaffe \r\npatch \r\nlaser \r\nframe \r\nparty \r\nlunch \r\nsweet \r\nstamp \r\nyoung \r\nagree \r\ntheme \r\nswipe \r\nworth \r\nharsh \r\nchild \r\npitch \r\nrebel \r\npaper \r\ntrick \r\nsleep \r\ntease \r\nwound \r\nbrake \r\ncheat \r\nproof \r\nreign \r\norder \r\nratio \r\nangle \r\namber \r\nsport \r\nraise \r\nlight \r\nmerit \r\nother \r\nmoney ";
+
+/***/ })
+
+}]);
---
+++ dev/src_wordfiles_Words_8L_txt24369df6cdae1a492757.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_Words_8L_txt"],{
+
+/***/ "./src/wordfiles/Words_8L.txt":
+/*!************************************!*\
+  !*** ./src/wordfiles/Words_8L.txt ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = "slippery \r\nbathroom \r\ncustomer \r\npredator \r\ngovernor \r\npersonal \r\nbulletin \r\nvariable \r\nrecovery \r\ncomposer \r\ndistrict \r\nminority \r\nincrease \r\ndefinite \r\nsunshine \r\nrational \r\ncompound \r\ninnocent \r\nheadline \r\ndisaster \r\neconobox \r\npositive \r\nignorant \r\nskeleton \r\ndiameter \r\ndividend \r\nfountain \r\nmourning \r\nreaction \r\ndiscount \r\nmushroom \r\ntraining \r\nrhetoric \r\nsickness \r\nmutation \r\ngradient \r\nsoftware \r\nreckless \r\nmisplace \r\ncylinder \r\nresident \r\nceremony \r\nmultiply \r\ncucumber \r\nnational \r\ndaughter \r\ndocument \r\nelection \r\ngraduate \r\noriginal ";
+
+/***/ })
+
+}]);
