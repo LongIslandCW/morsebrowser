9d33fb907a1b7431717e9b3d1e232bbd62de41c5
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2024-05-15 21:46:21 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@8611975e3bc01828ee8fc215b5b00814635398f0 🚀

---
+++ dev/src_wordfiles_2_Words_txt6cc135a5ec075ca137b8.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_2_Words_txt"],{
+
+/***/ "./src/wordfiles/2 Words.txt":
+/*!***********************************!*\
+  !*** ./src/wordfiles/2 Words.txt ***!
+  \***********************************/
+/***/ ((module) => {
+
+module.exports = "SEPARATE    PARAGRAPH  \r\nPARTICULAR    TEMPERATURE \r\nTRIANGLE    MATERIAL \r\nPOSSIBLE    QUESTION \r\nCOMPLETE    EXPERIENCE \r\nTHOUSAND    MOUNTAINS \r\nNECESSARY    EXPERIMENT \r\nDIFFICULT    SOLUTION \r\nMULTIPLY    FRACTIONS \r\nLANGUAGE    DICTIONARY \r\nOPPOSITE    CONDITION \r\nELECTRIC    SUBSTANCE \r\nREMEMBER    CHILDREN \r\nSUBTRACT    MATERIAL \r\nINDICATE    CONDITION \r\nEXERCISE    RESTRAINT \r\nINSTRUMENT    EXPERIMENT \r\nCONSIDER    PRACTICE \r\nREPRESENT    NECESSITY \r\nCHARACTER    REFERENCE \r\nNEIGHBORHOOD    SURPRISE \r\nPROPERTY    DIVISION \r\nPROBABLE    POSITION \r\nINTEREST    SUBTRACTION \r\nESPECIALLY    DIFFICULT ";
+
+/***/ })
+
+}]);
