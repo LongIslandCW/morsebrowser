28c97d6b683edb879d0e0f3908263d7b31dfe040
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2024-06-22 14:48:59 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@93822ec0c6a7a62261f00ff1444f27f8e6e2228a 🚀

---
+++ dev/src_wordfiles_INT1_1_Word_10_txt5dcc23831287b3ac0939.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_INT1_1_Word_10_txt"],{
+
+/***/ "./src/wordfiles/INT1_1 Word_10.txt":
+/*!******************************************!*\
+  !*** ./src/wordfiles/INT1_1 Word_10.txt ***!
+  \******************************************/
+/***/ ((module) => {
+
+module.exports = "drive {chef|} \r\n{|drive chef} \r\nhope {sure|} \r\n{|hope sure} \r\nlogic {equip|} \r\n{|logic equip} \r\nplay {yes|} \r\n{|play yes} \r\nnovel {only|} \r\n{|novel only} \r\ntrail {orbit|} \r\n{|trail orbit} \r\nsport {fear|} \r\n{|sport fear} \r\nski {taste|} \r\n{|ski taste} \r\nseek {tape|} \r\n{|seek tape} \r\nball {early|} \r\n{|ball early} ";
+
+/***/ })
+
+}]);
