2a23da391bb0e8c947fb3c89a56c7b2c296901b1
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2024-06-22 15:02:35 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@db4b23cb3aa6efed2e1dcf8c03fb1be433165e6c 🚀

---
+++ dev/src_wordfiles_INT2_1_Word_07_txt16137a82b6a281db120c.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_INT2_1_Word_07_txt"],{
+
+/***/ "./src/wordfiles/INT2_1 Word_07.txt":
+/*!******************************************!*\
+  !*** ./src/wordfiles/INT2_1 Word_07.txt ***!
+  \******************************************/
+/***/ ((module) => {
+
+module.exports = "quick {eight|} \r\n  {|quick eight} \r\nhome {auto|} \r\n  {|home auto} \r\nslip {adviser|} \r\n  {|slip adviser} \r\nbefore {salmon|} \r\n  {|before salmon} \r\ndress {whole|} \r\n  {|dress whole} \r\nequally {apple|} \r\n  {|equally apple} \r\ndepend {AIDS|} \r\n  {|depend AIDS} \r\nsound {path|} \r\n  {|sound path} \r\nangry {mess|} \r\n  {|angry mess} \r\nreal {memory|} \r\n  {|real memory} ";
+
+/***/ })
+
+}]);
---
+++ dev/src_wordfiles_INT2_2_Words_txtd8cb6b3c4b3cd19402de.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_INT2_2_Words_txt"],{
+
+/***/ "./src/wordfiles/INT2_2 Words.txt":
+/*!****************************************!*\
+  !*** ./src/wordfiles/INT2_2 Words.txt ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = "package  cheek\r\nfront  citizen\r\nsmile  anger\r\nplease  print\r\nAfrican  subject\r\nnotice  father\r\nscale  medium\r\nlack  slave\r\nsorry  track\r\nvision  similar\r\nthink  remark\r\nsure  dead\r\nsame  succeed\r\ndoor  agenda\r\nteam  twelve\r\nadmit  measure\r\ntend  exist\r\nshould  nose\r\nsimple  away\r\nstuff  acid\r\nafter  mass\r\nmarket  seize\r\nalbum  sharp\r\nhang  shelter\r\nassess  peer\r\nking  weekly\r\nface  sixth\r\nsuffer  bottom\r\nexpose  animal\r\nstop  thick\r\nquick  eight\r\nhome  auto\r\nslip  adviser\r\nbefore  salmon\r\ndress  whole\r\nequally  apple\r\ndepend  AIDS\r\nsound  path\r\nangry  mess\r\nreal  memory\r\ncoach  year\r\nquickly  media\r\nextent  advance\r\nphoto  outside\r\nleading  body\r\nmayor  also\r\ntruck  prime\r\nmajor  supply\r\nlibrary  target\r\nrequest  create\r\nrest  await\r\noccur  hope\r\never  welcome\r\nprofit  cloth\r\ncentral  excited\r\nnurse  cabin\r\nwell  blue\r\nscream  clean\r\nmystery  rapid\r\nmoon  lawsuit\r\njudge  burst\r\nscore  lightly\r\ndecade  hold\r\ngame  porch\r\nheavily  senior\r\naccuse  German\r\nsave  vitamin\r\nearth  sell\r\nlower  execute\r\nsuch  knee\r\nlead  battery\r\nrefer  emerge\r\nbond  suit\r\nlook  welfare\r\neach  lake\r\ndrag  sense\r\nchild  manual\r\nmember  prior\r\npenalty  towel\r\nbear  extreme\r\nentry  exhibit\r\nillness  chicken\r\nspread  shot\r\ndate  bell\r\nyell  proceed\r\nfrozen  fire\r\nretain  studio\r\nEnglish  throat\r\nfight  doll\r\ndirty  minimal\r\nforest  second\r\nodds  vaccine\r\nsigh  mouse\r\nbreath  length\r\ncool  spouse\r\ncloset  virus\r\njunior  subsidy\r\nbroken  ring\r\nbelief  from\r\nouter  recipe \r\n\r\n\r\n";
+
+/***/ })
+
+}]);
