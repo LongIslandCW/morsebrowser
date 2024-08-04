632f236546a177f1bc4c9abc8ac267b2cf69097f
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2024-08-04 18:32:25 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@19be75198141614615ab9743b8207cd7cd343d7c 🚀

---
+++ dev/src_wordfiles_SB2QXV5_txta12053907f00aca36fae.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_SB2QXV5_txt"],{
+
+/***/ "./src/wordfiles/SB2QXV5.txt":
+/*!***********************************!*\
+  !*** ./src/wordfiles/SB2QXV5.txt ***!
+  \***********************************/
+/***/ ((module) => {
+
+module.exports = "AL \nAR \nCA \nCO \nCT \n{DE|delaware} \nFL \n{GA|georgia} \n{HI|hawaii} \n{ID|idaho} \nIL \n{IN|indiana} \nIA \n{LA|louisiana} \nNE \nNV \nNH \nNC \nND \n{OH|ohio} \n{OR|oregon} \nPA \nRI \nSC \nSD \nTN \nTX \nUT \nVT \nVA \nWA \nWV \nWI \nAB \nBC \nNB \nNL \nNT \nNS \nNU \nON \nPE \nQC \n";
+
+/***/ })
+
+}]);
