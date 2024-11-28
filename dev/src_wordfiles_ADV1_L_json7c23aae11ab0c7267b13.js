58915d936c1916003c8eee9218530bf748a303c9
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2024-11-28 15:43:16 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@f7a69a54d6e1d906fc46e8d8df95de1ae297492b 🚀

---
+++ dev/src_wordfiles_ADV1_L_json7c23aae11ab0c7267b13.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_ADV1_L_json"],{
+
+/***/ "./src/wordfiles/ADV1_L.json":
+/*!***********************************!*\
+  !*** ./src/wordfiles/ADV1_L.json ***!
+  \***********************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"letters":"abcdefghijklmnopqrstuvwxyz","minWordSize":2,"maxWordSize":2,"practiceSeconds":120}');
+
+/***/ })
+
+}]);
---
+++ dev/src_wordfiles_ADV1_N_json52e8afa75f4bcf35c0d2.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_ADV1_N_json"],{
+
+/***/ "./src/wordfiles/ADV1_N.json":
+/*!***********************************!*\
+  !*** ./src/wordfiles/ADV1_N.json ***!
+  \***********************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"letters":"1234567890","minWordSize":2,"maxWordSize":2,"practiceSeconds":120}');
+
+/***/ })
+
+}]);
