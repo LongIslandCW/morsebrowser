e1547821c4fea0b8d3bbc5b59d851825234237c4
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2024-12-26 21:34:14 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@3ebeccfe840785dee25962844d8761585310aae6 🚀

---
+++ dev/src_wordfiles_Fam_N_json3e40aa348a445dcd1a6c.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_Fam_N_json"],{
+
+/***/ "./src/wordfiles/Fam_N.json":
+/*!**********************************!*\
+  !*** ./src/wordfiles/Fam_N.json ***!
+  \**********************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"letters":"1234567890","minWordSize":1,"maxWordSize":1,"practiceSeconds":120}');
+
+/***/ })
+
+}]);
