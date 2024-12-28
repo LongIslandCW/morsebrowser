832c6ec16fcc432fe45bea5410574c3a5b375e7c
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2024-12-28 18:09:03 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@ba2445eaabb4344763b6fd13eff410a95521b056 🚀

---
+++ dev/src_presets_sets_INT2_json26049b60b2e0d5691f83.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_presets_sets_INT2_json"],{
+
+/***/ "./src/presets/sets/INT2.json":
+/*!************************************!*\
+  !*** ./src/presets/sets/INT2.json ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"options":[{"display":"Default 12/12","filename":"INT2_DEFAULT_12.json"},{"display":"Default 16/16","filename":"INT2_Default_16.json"},{"display":"Affixes","filename":"INT2BUF.json"},{"display":"Call Signs","filename":"INT2_CS.json"},{"display":"Familiarity Phrases","filename":"INT2_FAM_Phrases.json"},{"display":"Familiarity Sentences","filename":"INT2_FAM_Sentences.json"},{"display":"Familiarity Spelling","filename":"INT2_FAM_Spell.json"},{"display":"Familiarity Words","filename":"INT2_FAM_Words.json"},{"display":"ICR","filename":"INT2_ICR.json"},{"display":"Multi Words","filename":"INT2_MW.json"},{"display":"Phrases","filename":"INT2_PHRASES.json"},{"display":"Recognition - Spell","filename":"INT2_REC_Spell.json"},{"display":"Recognition - Words","filename":"INT2_REC_Words.json"},{"display":"Sending Practice","filename":"INT2_SENDING.json"}]}');
+
+/***/ })
+
+}]);
