c04ea7c54e3ad90af6e9944a2d4d38c2476901fd
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-01-15 02:06:05 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@4d927cc411b578be2641494d6f372674729ff497 🚀

--- dev/src_presets_sets_ICR_json35010a6ef08921a95702.js
+++ dev/src_presets_sets_ICR_json35010a6ef08921a95702.js
@@ -1,10 +1,10 @@
 "use strict";
-(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_presets_sets_BUG_json"],{
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_presets_sets_ICR_json"],{
 
-/***/ "./src/presets/sets/BUG.json":
-/*!***********************************!*\
-  !*** ./src/presets/sets/BUG.json ***!
-  \***********************************/
+/***/ "./src/presets/sets/ICR+.json":
+/*!************************************!*\
+  !*** ./src/presets/sets/ICR+.json ***!
+  \************************************/
 /***/ ((module) => {
 
 module.exports = JSON.parse('{"options":[{"display":"Spell 12 wpm","filename":"ICR+_12.json"},{"display":"Words 12 wpm","filename":"Bug_12_Words.json"},{"display":"Spell 16 wpm","filename":"Bug_16.json"},{"display":"Words 16 wpm","filename":"Bug_16_Words.json"},{"display":"Spell 18 wpm","filename":"Bug_18.json"},{"display":"Words 18 wpm","filename":"Bug_18_Words.json"},{"display":"Spell 20 wpm","filename":"Bug_20.json"},{"display":"Words 20 wpm","filename":"Bug_20_Words.json"}]}');
