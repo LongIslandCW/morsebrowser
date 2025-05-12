3a094703090e2fbddf9b28a6a9b332572c99c612
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-05-12 14:54:09 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@0fa20e34b1be503773eb6780e9c156407c12b739 🚀

---
+++ dev/src_wordfiles_Com_test_jason_txtda8957bca6ea1b88ba4f.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_Com_test_jason_txt"],{
+
+/***/ "./src/wordfiles/Com_test.jason.txt":
+/*!******************************************!*\
+  !*** ./src/wordfiles/Com_test.jason.txt ***!
+  \******************************************/
+/***/ ((module) => {
+
+module.exports = "{\r\n     \"letters\": \"abcdefghijklmnopqrstuvwxyz1234567890 <sk> <bt> / , . ? <ar> <bk> \",\r\n     \"minWordSize\": 5,\r\n     \"maxWordSize\": 5,\r\n     \"practiceSeconds\" : 120\r\n\r\n }";
+
+/***/ })
+
+}]);
