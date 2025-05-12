4760c2bad5f1c76830f551588713d52b53f8a710
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-05-12 15:18:29 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@9a4551690758c40a990637811751812b03e50a56 🚀

---
+++ dev/src_presets_sets_Com_test_Preset_Group_json5cd6a71e7a8b0bf2c68d.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_presets_sets_Com_test_Preset_Group_json"],{
+
+/***/ "./src/presets/sets/Com_test Preset Group.json":
+/*!*****************************************************!*\
+  !*** ./src/presets/sets/Com_test Preset Group.json ***!
+  \*****************************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"options":[{"display":"20 wpm","filename":"Com_test_Preset.json"}]}');
+
+/***/ })
+
+}]);
