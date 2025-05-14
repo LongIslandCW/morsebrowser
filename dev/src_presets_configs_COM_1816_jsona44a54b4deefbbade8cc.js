70e6ffd619764f3094001d76eac6a90b7b19beeb
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-05-14 19:25:55 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@bb09dee7b46d4f81e1e4661ac0a19bac09b0bf31 🚀

---
+++ dev/src_presets_configs_COM_1816_jsona44a54b4deefbbade8cc.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_presets_configs_COM_1816_json"],{
+
+/***/ "./src/presets/configs/COM_1816.json":
+/*!*******************************************!*\
+  !*** ./src/presets/configs/COM_1816.json ***!
+  \*******************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"morseSettings":[{"key":"wpm","value":18,"comment":null},{"key":"fwpm","value":16,"comment":null},{"key":"xtraWordSpaceDits","value":"2","comment":null},{"key":"volume","value":"4","comment":null},{"key":"stickySets","value":"BK","comment":null},{"key":"ifStickySets","value":false,"comment":null},{"key":"syncWpm","value":false,"comment":null},{"key":"hideList","value":false,"comment":null},{"key":"showRaw","value":true,"comment":null},{"key":"autoCloseLessonAccordian","value":false,"comment":null},{"key":"customGroup","value":"","comment":null},{"key":"showExpertSettings","value":"true","comment":null},{"key":"voiceEnabled","value":true,"comment":null},{"key":"voiceSpelling","value":false,"comment":null},{"key":"voiceThinkingTime","value":0.5,"comment":null},{"key":"voiceAfterThinkingTime","value":"0","comment":null},{"key":"voiceVolume","value":10,"comment":null},{"key":"voiceLastOnly","value":false,"comment":null},{"key":"voiceRecap","value":false,"comment":null},{"key":"speakFirst","value":false,"comment":null},{"key":"numberOfRepeats","value":0,"comment":null},{"key":"speakFirstAdditionalWordspaces","value":0,"comment":null},{"key":"keepLines","value":false,"comment":null},{"key":"syncSize","value":true,"comment":null},{"key":"overrideSize","value":true,"comment":null},{"key":"overrideSizeMin","value":5,"comment":null},{"key":"overrideSizeMax","value":5,"comment":null},{"key":"cardSpace","value":0,"comment":"AKA cardWait"},{"key":"miscSettingsAccordionOpen","value":"true","comment":null},{"key":"speedInterval","value":false,"comment":null},{"key":"intervalTimingsText","value":"","comment":null},{"key":"intervalWpmText","value":"","comment":null},{"key":"intervalFwpmText","value":"","comment":null},{"key":"voiceBufferMaxLength","value":1,"comment":null}]}');
+
+/***/ })
+
+}]);
