b9918fcb1f817a1374723c7bc94eb4efb3edba59
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-05-14 19:19:40 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@0ade665629c4fb03b898285c47f16f3c9a34d244 🚀

--- dev/src_presets_sets_COM_jsonadad7d6579baff48dfeb.js
+++ dev/src_presets_sets_COM_jsonadad7d6579baff48dfeb.js
@@ -7,7 +7,7 @@
   \***********************************/
 /***/ ((module) => {
 
-module.exports = JSON.parse('{"options":[{"display":"20 wpm","filename":"Com_test_Preset.json"}]}');
+module.exports = JSON.parse('{"options":[{"display":"18/16 wpm","filename":"COM_1816.json"},{"display":"20/16 wpm","filename":"COM_2016.json"},{"display":"22/16 wpm","filename":"COM_2216.json"},{"display":"25/16 wpm","filename":"COM_2516.json"},{"display":"30/16 wpm","filename":"COM_3016.json"}]}');
 
 /***/ })
 
