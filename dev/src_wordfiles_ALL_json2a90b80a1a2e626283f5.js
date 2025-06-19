20be8f3a375ab5f5d219e9c8e025fa13fcdad7da
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-06-19 21:52:10 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@ad31b7dc12765a6e0885e9a3e9ff348bfd7be3fa 🚀

---
+++ dev/src_wordfiles_ALL_json2a90b80a1a2e626283f5.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_ALL_json"],{
+
+/***/ "./src/wordfiles/ALL.json":
+/*!********************************!*\
+  !*** ./src/wordfiles/ALL.json ***!
+  \********************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"letters":"abcdefghijklmnopqrstuvwxyz1234567890<sk><bt>/,.?<ar><bk>","minWordSize":3,"maxWordSize":3,"practiceSeconds":120}');
+
+/***/ })
+
+}]);
