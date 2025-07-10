5ac546484884de7befde2baf06e94d1f80017da5
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-07-10 20:39:33 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@136bff6731565126f68eed9de433d15da763b61a 🚀

---
+++ dev/src_presets_sets_POL_jsonf55e9cbc8d941c013c86.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_presets_sets_POL_json"],{
+
+/***/ "./src/presets/sets/POL.json":
+/*!***********************************!*\
+  !*** ./src/presets/sets/POL.json ***!
+  \***********************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"options":[{"display":"BINOMIALS 27 wpm","filename":"POL_27_BI.json"},{"display":"BINOMIALS 31 wpm","filename":"POL_31_BI.json"},{"display":"CHARACTERS 27 wpm","filename":"POL_27.json"},{"display":"WORDS 27 wpm","filename":"POL_27_WORDS.json"},{"display":"CHARACTERS 31 wpm","filename":"POL_31.json"},{"display":"WORDS 31 wpm","filename":"POL_31_WORDS.json"}]}');
+
+/***/ })
+
+}]);
