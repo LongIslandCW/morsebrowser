224b0cf7d8038052b572bfadee1b29991a152e51
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-07-23 14:30:50 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@52851b5b58844bf9afa3103166c732fe9d0d8944 🚀

---
+++ dev/src_wordfiles_POL_2W_ALL_txt42578ad5a74312dc426e.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_2W_ALL_txt"],{
+
+/***/ "./src/wordfiles/POL_2W_ALL.txt":
+/*!**************************************!*\
+  !*** ./src/wordfiles/POL_2W_ALL.txt ***!
+  \**************************************/
+/***/ ((module) => {
+
+module.exports = "AAA \r\nAM \r\nAS \r\nAT \r\nAN \r\nAX \r\nAD \r\nAG \r\nBBB \r\nBE \r\nBY \r\nDDD \r\nDO \r\nDE \r\nEEE \r\nEX \r\nES \r\nEW \r\nGGG \r\nGO \r\nHHH \r\nHE \r\nHI \r\nHA \r\nIII \r\nIF \r\nIN \r\nIT \r\nIS \r\nID \r\nLLL \r\nLO \r\nMMM \r\nME \r\nMY \r\nMA \r\nNNN \r\nNO \r\nOOO \r\nOF \r\nOK \r\nOH \r\nON \r\nOR \r\nOX \r\nOP \r\nOM \r\nPPP \r\nPA \r\nPI \r\nSSS \r\nSO \r\nTTT \r\nTO \r\nUUU \r\nUS \r\nUP \r\nUM \r\nWWW \r\nWE \r\n";
+
+/***/ })
+
+}]);
