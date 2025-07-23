414451a25a4f51f06bc72df2d3b015432b5d36e2
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-07-23 13:11:55 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@948687334738db7a38b603100eed0338524cd5be 🚀

---
+++ dev/src_wordfiles_POL_2WPhrases_txtb344500a01ed6e8c8610.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_2WPhrases_txt"],{
+
+/***/ "./src/wordfiles/POL_2WPhrases.txt":
+/*!*****************************************!*\
+  !*** ./src/wordfiles/POL_2WPhrases.txt ***!
+  \*****************************************/
+/***/ ((module) => {
+
+module.exports = "AAA \r\nAS IF \r\nANT IS \r\nALL IN \r\nBBB \r\nBAD DOG \r\nBE KIND \r\nBE SAFE \r\nCCC \r\nCAN DO \r\nCAR DOOR \r\nCASH FLOW \r\nDDD \r\nDO WELL \r\nDID YOU \r\nDONT WAIT \r\nEEE \r\nEAT NOW \r\nEGG ROLL \r\nEASY COPY \r\nFFF \r\nFB \r\nFEAR NOT \r\nFIX THAT \r\nGGG \r\nGO TO \r\nGOT IT \r\nGO AWAY \r\nHHH \r\nHAVE FUN \r\nHIT SONG \r\nHOT STOVE \r\nIII \r\nI CAN \r\nIF NOT \r\nITS OKAY \r\nJJJ \r\nJUST FINE \r\nJET PLANE \r\nJINX IT \r\nKKK \r\nKITE FLY \r\nKICK BACK \r\nKEEP CALM \r\nLLL \r\nLETS GO \r\nLINK UP \r\nLAST TRY \r\nMMM \r\nMUST GO \r\nMY GOSH \r\nMAKE NICE \r\nNNN \r\nNOT ME \r\nNEW KEY \r\nNICE DAY \r\nOOO \r\nOP IS \r\nOWN IT \r\nOR ELSE \r\nPPP \r\nPOP QUIZ \r\nPET CAT \r\nPSE QRS \r\nQQQ \r\nQTH IS \r\nQUIT THAT \r\nQUICK JOB \r\nRRR \r\nRST IS \r\nRED FOX \r\nRAT RACE \r\nSSS \r\nSO WHAT \r\nSIT UP \r\nSET DOWN \r\nTTT \r\nTOP TEN \r\nTOO LATE \r\nTIP TOP \r\nUUU \r\nUS TOO \r\nUSE CASE \r\nUP THERE \r\nVVV \r\nVERY COOL \r\nVOLT METER \r\nVICE GRIPS \r\nWWW \r\nWHO ME? \r\nWHAT NOW \r\nWORK DAY \r\nXXX \r\nXYL WIFE \r\nXMAS WEEK \r\nXRAY VISION \r\nYYY \r\nYOU CAN \r\nYES PLEASE \r\nYIELD SIGN \r\nZZZ \r\nZIG ZAG \r\nZOOM CLASS \r\nZEN MINDSET \r\n";
+
+/***/ })
+
+}]);
