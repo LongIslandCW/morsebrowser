d3e2abed5443f80b13da75366371f54a8cc965b5
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-07-29 19:59:15 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@503ad437b94941ca7c55b943cb1b53fa7c691239 🚀

--- dev/src_wordfiles_POL_ED_txtf27a2076e6ac6168b8fb.js
+++ dev/src_wordfiles_POL_ED_txtf27a2076e6ac6168b8fb.js
@@ -7,7 +7,7 @@
   \**********************************/
 /***/ ((module) => {
 
-module.exports = "AXED \r\nBED \r\nICED \r\nDYED \r\nEYED \r\nFED \r\nAGED \r\nHID \r\nTIED \r\nJOGGED \r\nKEYED \r\nLED \r\nMIXED \r\nNEED \r\nOILED \r\nPAID \r\nQUIZZED \r\nRED \r\nSUED \r\nTIED \r\nUSED \r\nVIED \r\nWED \r\nVEXED \r\nYELLED \r\nZONED \r\n";
+module.exports = "ED \r\nAXED \r\nBED \r\nICED \r\nDYED \r\nEYED \r\nFED \r\nAGED \r\nHID \r\nTIED \r\nJOGGED \r\nKEYED \r\nLED \r\nMIXED \r\nNEED \r\nOILED \r\nPAID \r\nQUIZZED \r\nRED \r\nSUED \r\nTIED \r\nUSED \r\nVIED \r\nWED \r\nVEXED \r\nYELLED \r\nZONED \r\nED \r\n";
 
 /***/ })
 
