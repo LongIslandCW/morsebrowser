6fe75608d0457d2f3295407622ed7bdb78c7988b
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-07-30 20:49:16 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@605e6fc22a2d2e7e696519b3a8835a5e62baf550 🚀

---
+++ dev/src_wordfiles_POL_ASS2_txt8991ece6a9a146af2e74.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_ASS2_txt"],{
+
+/***/ "./src/wordfiles/POL_ASS2.txt":
+/*!************************************!*\
+  !*** ./src/wordfiles/POL_ASS2.txt ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = "VVV \r\nL D G U H B C F P \r\nLOG SONG OLIVE \r\nBIG AND SMALL \r\nDON'T GIVE UP \r\n";
+
+/***/ })
+
+}]);
