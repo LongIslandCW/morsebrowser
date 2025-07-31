058d7832717aaf54a0dbd7edec1c94ae950aa20e
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-07-31 02:53:36 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@8207bff04f0bba6cbd97bc593eaa5add7936aa3a 🚀

---
+++ dev/src_presets_sets_A_jsonb795795bb998d6323f44.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_presets_sets_A_json"],{
+
+/***/ "./src/presets/sets/A.json":
+/*!*********************************!*\
+  !*** ./src/presets/sets/A.json ***!
+  \*********************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"options":[{"display":"Flow Rate 1","filename":"POL_1.json"},{"display":"Flow Rate 2","filename":"POL_2.json"},{"display":"Flow Rate 3","filename":"POL_3.json"}]}');
+
+/***/ })
+
+}]);
