e63cc15b9b72b239d212fe5dc0ce0332403f6c90
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-08-01 19:05:34 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@6d9d25a663f27e72386f5dc9963df792ba0c1142 🚀

---
+++ dev/src_wordfiles_POL_Letters_ALT_txt7c55af3eccf38044456a.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_Letters_ALT_txt"],{
+
+/***/ "./src/wordfiles/POL_Letters_ALT.txt":
+/*!*******************************************!*\
+  !*** ./src/wordfiles/POL_Letters_ALT.txt ***!
+  \*******************************************/
+/***/ ((module) => {
+
+module.exports = "A \r\nB \r\nC \r\nD \r\nE \r\nF \r\nG \r\nH \r\nI \r\nJ \r\nK \r\nL \r\nM \r\nN \r\nO \r\nP \r\nQ \r\nR \r\nS \r\nT \r\nU \r\nV \r\nW \r\nX \r\nY \r\nZ  \r\nAAA \r\nBBB \r\nCCC \r\nABC \r\nDDD \r\nEEE \r\nFFF \r\nDEF \r\nGGG \r\nHHH \r\nIII \r\nGHI \r\nJJJ \r\nKKK \r\nLLL \r\nJKL \r\nMMM \r\nNNN \r\nOOO \r\nMNO \r\nPPP \r\nQQQ \r\nRRR \r\nPQR \r\nSSS \r\nTTT \r\nUUU \r\nSTU \r\nVVV \r\nWWW \r\nXXX \r\nVWX \r\nYYY \r\nZZZ \r\nXYZ \r\nABC \r\nDEF \r\nGHI \r\nJKL \r\nMNO \r\nPQR \r\nSTU \r\nVWX \r\nXYZ \r\n\r\n";
+
+/***/ })
+
+}]);
