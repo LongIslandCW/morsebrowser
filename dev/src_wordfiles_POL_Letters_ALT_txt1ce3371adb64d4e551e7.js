e8f84cb757a2238afa692994ea7a58fe858207fa
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-08-01 18:42:42 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@245dc827b4e2675f3895a50ce29821cd33847cc0 🚀

---
+++ dev/src_wordfiles_POL_Letters_ALT_txt1ce3371adb64d4e551e7.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_Letters_ALT_txt"],{
+
+/***/ "./src/wordfiles/POL_Letters_ALT.txt":
+/*!*******************************************!*\
+  !*** ./src/wordfiles/POL_Letters_ALT.txt ***!
+  \*******************************************/
+/***/ ((module) => {
+
+module.exports = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z \r\nAAA \r\nBBB \r\nCCC \r\nABC \r\nDDD \r\nEEE \r\nFFF \r\nDEF \r\nGGG \r\nHHH \r\nIII \r\nGHI \r\nJJJ \r\nKKK \r\nLLL \r\nJKL \r\nMMM \r\nNNN \r\nOOO \r\nMNO \r\nPPP \r\nQQQ \r\nRRR \r\nPQR \r\nSSS \r\nTTT \r\nUUU \r\nSTU \r\nVVV \r\nWWW \r\nXXX \r\nVWX \r\nYYY \r\nZZZ \r\nXYZ \r\nABC \r\nDEF \r\nGHI \r\nJKL \r\nMNO \r\nPQR \r\nSTU \r\nVWX \r\nXYZ \r\n\r\n";
+
+/***/ })
+
+}]);
