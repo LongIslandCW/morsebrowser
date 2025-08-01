fbd2a8fa7997fcf5b15fbbb78cd64c803b5148da
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-08-01 18:52:38 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@a64ff610493181eca2da2e4b4ee971e671d6464a 🚀

---
+++ dev/src_wordfiles_POL_Numbers_txtdb3b56b4c7cfa326c85e.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_Numbers_txt"],{
+
+/***/ "./src/wordfiles/POL_Numbers.txt":
+/*!***************************************!*\
+  !*** ./src/wordfiles/POL_Numbers.txt ***!
+  \***************************************/
+/***/ ((module) => {
+
+module.exports = "1 2 3 4 5 6 7 8 9 0 \r\n111 \r\n222 \r\n333 \r\n444 \r\n555 \r\n666 \r\n777 \r\n888 \r\n999 \r\n000 \r\n1 2 3 4 5 6 7 8 9 0 \r\n";
+
+/***/ })
+
+}]);
---
+++ dev/src_wordfiles_POL_SENDING_L_txt2a9ff1111c911727daec.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_SENDING_L_txt"],{
+
+/***/ "./src/wordfiles/POL_SENDING_L.txt":
+/*!*****************************************!*\
+  !*** ./src/wordfiles/POL_SENDING_L.txt ***!
+  \*****************************************/
+/***/ ((module) => {
+
+module.exports = "AAA \r\nBBB \r\nCCC \r\nDDD \r\nEEE \r\nFFF \r\nGGG \r\nHHH \r\nIII \r\nJJJ \r\nKKK \r\nLLL \r\nMMM \r\nNNN \r\nOOO \r\nPPP \r\nQQQ \r\nRRR \r\nSSS \r\nTTT \r\nUUU \r\nVVV \r\nWWW \r\nXXX \r\nYYY \r\nZZZ \r\n";
+
+/***/ })
+
+}]);
