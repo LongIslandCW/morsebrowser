dc0aa905eeec843e7ce6d450d8d76b75304fdec5
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-08-16 21:46:45 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@4dd1268ae56b39960e92098f7f5501dfffc489c7 🚀

---
+++ dev/src_presets_sets_POL__json471c647a5f62de2f7aa6.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_presets_sets_POL__json"],{
+
+/***/ "./src/presets/sets/POL_.json":
+/*!************************************!*\
+  !*** ./src/presets/sets/POL_.json ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"options":[{"display":"CHARACTERS Flow Rate 1","filename":"POL_27.json"},{"display":"CHARACTERS Flow Rate 2","filename":"POL_31.json"},{"display":"WORDS Flow Rate 1","filename":"POL_27_WORDS.json"},{"display":"WORDS Flow Rate 2","filename":"POL_31_WORDS.json"},{"display":"BINOMIALS Flow Rate 1","filename":"POL_27_BI.json"},{"display":"BINOMIALS Flow Rate 2","filename":"POL_31_BI.json"},{"display":"SENDING ALPHABET Flow Rate 1","filename":"POL_27_SA.json"},{"display":"SENDING ALPHABET Flow Rate 2","filename":"POL_31_SA.json"},{"display":"SENDING NUMBERS Flow Rate 1","filename":"POL_27_SN.json"},{"display":"SENDING NUMBERS Flow Rate 2","filename":"POL_31_SN.json"}]}');
+
+/***/ })
+
+}]);
