a690a08ee63820973c1815237ca3fa49aba59742
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-08-16 21:01:59 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@1c21af59ceed96694439f43b912343e0fb1f929a 🚀

---
+++ dev/src_wordfiles_POL_Letters_txt43c94a75fc4f80b03ba7.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_Letters_txt"],{
+
+/***/ "./src/wordfiles/POL_Letters.txt":
+/*!***************************************!*\
+  !*** ./src/wordfiles/POL_Letters.txt ***!
+  \***************************************/
+/***/ ((module) => {
+
+module.exports = "[ ]A[ ]B[ ]C[ ]D[ ]E[ ]F[ ]G[ ]H[ ]I[ ]J[ ]K[ ]L[ ]M[ ]N[ ]O[ ]P[ ]Q[ ]R[ ]S[ ]T[ ]U[ ]V[ ]W[ ]X[ ]Y[ ]Z \r\nAAA AAA AAA \r\nBBB BBB BBB \r\nCCC CCC CCC \r\nABC ABC ABC \r\nDDD DDD DDD \r\nEEE EEE EEE \r\nFFF FFF FFF \r\nDEF DEF DEF \r\nGGG GGG GGG \r\nHHH HHH HHH \r\nIII III III \r\nGHI GHI GHI \r\nJJJ JJJ JJJ \r\nKKK KKK KKK \r\nLLL LLL LLL \r\nJKL JKL JKL \r\nMMM MMM MMM \r\nNNN NNN NNN \r\nOOO OOO OOO \r\nMNO MNO MNO \r\nPPP PPP PPP \r\nQQQ QQQ QQQ \r\nRRR RRR RRR \r\nPQR PQR PQR \r\nSSS SSS SSS \r\nTTT TTT TTT \r\nUUU UUU UUU \r\nSTU STU STU \r\nVVV VVV VVV \r\nWWW WWW WWW \r\nXXX XXX XXX \r\nVWX VWX VWX \r\nYYY YYY YYY \r\nZZZ ZZZ ZZZ \r\nXYZ XYZ XYZ \r\n[   ]ABC[   ]DEF[   ]GHI[   ]JKL[   ]MNO[   ]PQR[   ]STU[   ]VWX[   ]XYZ \r\n[   ]ABC[  ]DEF[  ]GHI[  ]JKL[  ]MNO[  ]PQR[  ]STU[  ]VWX[  ]XYZ \r\n\r\n[   ]ABC[ ]DEF[ ]GHI[ ]JKL[ ]MNO[ ]PQR[ ]STU[ ]VWX[ ]XYZ \r\n";
+
+/***/ })
+
+}]);
