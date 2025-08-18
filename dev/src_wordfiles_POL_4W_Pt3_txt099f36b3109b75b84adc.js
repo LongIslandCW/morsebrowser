e8ba422ec26d5b709fa5df7c240bab56b53021ce
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-08-18 13:11:46 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@4b9c595ba2187b54c8b645c93cb02dc84dd34200 🚀

---
+++ dev/src_wordfiles_POL_4W_Pt3_txt099f36b3109b75b84adc.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_4W_Pt3_txt"],{
+
+/***/ "./src/wordfiles/POL_4W_Pt3.txt":
+/*!**************************************!*\
+  !*** ./src/wordfiles/POL_4W_Pt3.txt ***!
+  \**************************************/
+/***/ ((module) => {
+
+module.exports = "AAA \r\nASAP \r\nACID \r\nAIRY \r\nBBB \r\nBRAG \r\nBOAT \r\nBEND \r\nCCC \r\nCOME \r\nCAGE \r\nCAFE \r\nDDD \r\nDROP \r\nDAFT \r\nDOZE \r\nDECK \r\nEEE \r\nEVEN \r\nEAST \r\nEARN \r\nFFF \r\nFALL \r\nFLIP \r\nFROG \r\nGGG \r\nGOLF \r\nGOLD \r\nGATE \r\nHHH \r\nHAWK \r\nHERO \r\nHUNT \r\nIII \r\nICON \r\nINTO \r\nIDEA \r\nJJJ \r\nJUMP \r\nJUST \r\nJEST \r\nKKK \r\nKICK \r\nKINK \r\nKNOB \r\nLLL \r\nLEFT \r\nLADY \r\nLAZY \r\nMMM \r\nMEMO \r\nMANY \r\nMADE \r\nNNN \r\nNICE \r\nNEAT \r\nNOTE \r\nOOO \r\nOVEN \r\nOBEY \r\nOOZE \r\nPPP \r\nPICK \r\nPAIL \r\nPINT \r\nQQQ \r\nQUIP \r\nQRS \r\nQSY \r\nRRR \r\nROAD \r\nRAIN \r\nRIDE \r\nSSS \r\nSAIL \r\nSING \r\nSAID \r\nTTT \r\nTHEN \r\nTHEM \r\nTELL \r\nUUU \r\nUSED \r\nURGE \r\nUPON \r\nUNDO \r\nVVV \r\nVOTE \r\nVAIN \r\nVAST \r\nWWW \r\nWAGE \r\nWORK \r\nWELL \r\nXXX \r\nXRAY \r\nXCVR \r\nXBOX \r\nYYY \r\nYAGI \r\nYARN \r\nYELL \r\nZZZ \r\nZIPS \r\nZEST \r\nZOOM \r\n\r\n";
+
+/***/ })
+
+}]);
