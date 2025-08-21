569ea166acfe460c94fbcb6b48f1c34d840fde6f
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-08-21 01:06:38 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@43e3b09cc276ed651e6454ea28f7fd3f42cfce5d 🚀

---
+++ dev/src_wordfiles_POL_S_Phrases_Pt2_txt9e512958855a13c93e09.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_S_Phrases_Pt2_txt"],{
+
+/***/ "./src/wordfiles/POL_S_Phrases_Pt2.txt":
+/*!*********************************************!*\
+  !*** ./src/wordfiles/POL_S_Phrases_Pt2.txt ***!
+  \*********************************************/
+/***/ ((module) => {
+
+module.exports = "[ ] AAA \r\nAN OLD MIC \r\nANT INV VEE \r\nBBB \r\nBE THE ONE \r\nBY THE TIME \r\nCCC \r\nCAT IS OUT \r\nCAN HE RUN \r\nDDD \r\nDO NOT GO \r\nDONT DO THAT \r\nEEE \r\nEND FED ANT \r\nEAT MORE ELK \r\nFFF \r\nFEED MY PIG \r\nFEEL LOST NOW \r\nGGG \r\nGET A NAP \r\nGOT THE STORY  \r\nHHH \r\nHOT FOR NOW \r\nHIT THE SACK \r\nIII \r\nI DID WELL \r\nITS NOT DUE \r\nJJJ \r\nJAR OF JAM \r\nJUST FOR FUN \r\nKKK \r\nKEPT IT ALL \r\nKEG IS FULL \r\nLLL \r\nLET IT GO \r\nLIVE IT NOW \r\nMMM \r\nMUD ON RUG \r\nMY TIN CUP \r\nNNN \r\nNEW TO ME \r\nNOW ITS OVER \r\nOOO \r\nOUR OLD RUG \r\nOWN THE VAN \r\nPPP \r\nPAL OF MINE \r\nPAINT THE WALL \r\nQQQ \r\nQUIP WAS GOOD \r\nQUIT IT NOW \r\nRRR \r\nRIP IT OFF \r\nRUN FOR IT \r\nSSS \r\nSHE CAN WAIT \r\nSIX TON VAN \r\nTTT \r\nTHE TIN MAN \r\nTIP TOP JOB \r\nUUU \r\nUP TO NOW \r\nUSE IT UP \r\nVVV \r\nVAN FOR SIX \r\nVISA CARD BILL \r\nWWW \r\nWHO ARE YOU \r\nWAX THE CAR  \r\nXXX \r\nXYL IS HERE \r\nXRAYS ARE FUN \r\nYYY \r\nYARD IS BIG \r\nYOU CAN PAY \r\nZZZ \r\nZIP THE COAT \r\nZOOM ROOM LINK \r\n\r\n";
+
+/***/ })
+
+}]);
