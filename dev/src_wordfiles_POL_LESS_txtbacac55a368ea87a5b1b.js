cbe5121be456e6c2201fd16d5481833a13fe991c
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-08-21 20:43:44 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@770de2e3bfd1cf20eed4bfa94daab697b9adcf10 🚀

---
+++ dev/src_wordfiles_POL_LESS_txtbacac55a368ea87a5b1b.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_LESS_txt"],{
+
+/***/ "./src/wordfiles/POL_LESS.txt":
+/*!************************************!*\
+  !*** ./src/wordfiles/POL_LESS.txt ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = "[ ] AAA \r\nAIMLESS \r\nARTLESS \r\nBBB \r\nBASELESS \r\nBOUNDLESS \r\nCCC \r\nCARELESS \r\nCLUELESS \r\nDDD \r\nDATELESS \r\nDOUBTLESS \r\nEEE \r\nENDLESS \r\nEFFORTLESS \r\nFFF \r\nFAULTLESS \r\nFEARLESS \r\nGGG \r\nGRACELESS \r\nGROUNDLESS \r\nHHH \r\nHAPLESS \r\nHOPELESS \r\nIII \r\nILLNESS \r\nICELESS \r\nJJJ \r\nJOYLESS \r\nJOBLESS \r\nKKK \r\nKNOTLESS \r\nKEYLESS \r\nLLL \r\nLIFELESS \r\nLAWLESS \r\nMMM \r\nMINDLESS  \r\nMATCHLESS \r\nNNN \r\nNAMELESS \r\nNEEDLESS \r\nOOO \r\nODORLESS \r\nOWNERLESS \r\nPPP \r\nPITILESS \r\nPOWERLESS \r\nQQQ \r\nQUARRELLESS  \r\nQUESTIONLESS \r\nRRR \r\nRUTHLESS \r\nRESTLESS \r\nSSS \r\nSHAMELESS \r\nSPINELESS \r\nTTT \r\nTIRELESS \r\nTHOUGHTLESS \r\nUUU \r\nUNSELFLESS \r\nUSELESS \r\nVVV \r\nVALUELESS \r\nVOICELESS \r\nWWW \r\nWEIGHTLESS \r\nWORDLESS \r\nYYY \r\nYOUTHLESS \r\nZZZ \r\nZONELESS \r\n";
+
+/***/ })
+
+}]);
---
+++ dev/src_wordfiles_POL_OUS_txte4e7471d1b6cefebcdc2.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_OUS_txt"],{
+
+/***/ "./src/wordfiles/POL_OUS.txt":
+/*!***********************************!*\
+  !*** ./src/wordfiles/POL_OUS.txt ***!
+  \***********************************/
+/***/ ((module) => {
+
+module.exports = "[ ] AAA \r\nANXIOUS \r\nAMOROUS \r\nBBB \r\nBOGUS \r\nBILIOUS \r\nCCC \r\nCURIOUS \r\nCAUTIOUS \r\nDDD \r\nDUBIOUS \r\nEEE \r\nENVIOUS \r\nEGREGIOUS \r\nFFF \r\nFAMOUS \r\nFABULOUS \r\nGGG \r\nGRACIOUS \r\nGLORIOUS \r\nHHH \r\nHUMOROUS \r\nHAZARDOUS \r\nIII \r\nINFAMOUS \r\nINJURIOUS \r\nJJJ \r\nJEALOUS \r\nJOYOUS \r\nLLL \r\nLUSCIOUS \r\nLUXURIOUS \r\nMMM \r\nMALICIOUS \r\nMONSTROUS \r\nNNN \r\nNERVOUS \r\nNOXIOUS \r\nOOO \r\nOBVIOUS \r\nPPP \r\nPOROUS \r\nPERILOUS \r\nQQQ \r\nQUERULOUS \r\nRRR \r\nRIGOROUS \r\nRIGHTEOUS \r\nSSS \r\nSERIOUS \r\nSPACIOUS \r\nTTT \r\nTEDIOUS \r\nTENUOUS \r\nVVV \r\nVARIOUS \r\nVIRTUOUS \r\nWWW \r\nWONDROUS \r\nZZZ \r\nZEALOUS \r\n";
+
+/***/ })
+
+}]);
