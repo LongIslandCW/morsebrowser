d04da55bd39fd978d82b62e46b6a144568bd8bc9
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-08-26 20:44:45 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@cf2c4a5645ffb3e898a44c842601842e31ad3025 🚀

---
+++ dev/src_wordfiles_INT1_Protocal_4C_txt29dbcc63dca24288a0e8.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_INT1_Protocal_4C_txt"],{
+
+/***/ "./src/wordfiles/INT1_Protocal_4C.txt":
+/*!********************************************!*\
+  !*** ./src/wordfiles/INT1_Protocal_4C.txt ***!
+  \********************************************/
+/***/ ((module) => {
+
+module.exports = "OK JERRY TNX FER FB QSO \r\nES HP CUAGN 73 AR \r\nK7YWX DE W6TDP TU SK \r\nOK MIKE TNX FER FB QSO \r\nES HP CUAGN 73 AR ";
+
+/***/ })
+
+}]);
