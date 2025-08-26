e519176ac3f699e6f04e264d95b5c46bb038ff4e
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-08-26 20:55:46 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@4d381dce7273d4c17c0b6a0e07d4a78116b93610 🚀

---
+++ dev/src_wordfiles_INT1_Bi_5_txt3ffe173398d6bede7c6a.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_INT1_Bi_5_txt"],{
+
+/***/ "./src/wordfiles/INT1_Bi_5.txt":
+/*!*************************************!*\
+  !*** ./src/wordfiles/INT1_Bi_5.txt ***!
+  \*************************************/
+/***/ ((module) => {
+
+module.exports = "DOS AND DONTS \r\nPUSH AND PULL \r\nJOIN OR LEAVE \r\nNULL AND VOID \r\nBRIGHT AND EARLY \r\nSAFE AND SOUND \r\nBACK AND FORTH \r\nTWISTS AND TURNS \r\nCOME AND GO \r\nEAT AND DRINK \r\n";
+
+/***/ })
+
+}]);
