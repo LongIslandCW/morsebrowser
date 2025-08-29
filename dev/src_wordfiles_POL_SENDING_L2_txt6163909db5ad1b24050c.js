026755e3867ee9b3100f96da6ef4230d2c4403b9
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-08-29 20:31:47 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@4fe752887dae9d26bd02743086d2ac610dd78589 🚀

---
+++ dev/src_wordfiles_POL_SENDING_L2_txt6163909db5ad1b24050c.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_SENDING_L2_txt"],{
+
+/***/ "./src/wordfiles/POL_SENDING_L2.txt":
+/*!******************************************!*\
+  !*** ./src/wordfiles/POL_SENDING_L2.txt ***!
+  \******************************************/
+/***/ ((module) => {
+
+module.exports = "A \r\nB \r\nC \r\nD \r\nE \r\nF \r\nG \r\nH \r\nI \r\nJ \r\nK \r\nL \r\nM \r\nN \r\nO \r\nP \r\nQ \r\nR \r\nS \r\nT \r\nU \r\nV \r\nW \r\nX \r\nY \r\nZ \r\nABC[    ] \r\nDEF[    ] \r\nGHI[    ]  \r\nJKL[    ] \r\nMNO[    ] \r\nPQR[    ] \r\nSTU[    ] \r\nVWX[    ] \r\nXYZ[    ] \r\nABCDEF[           ] \r\nGHIJKL[           ] \r\nMNOPQR[           ] \r\nSTUVWX[           ] \r\nYZ ";
+
+/***/ })
+
+}]);
