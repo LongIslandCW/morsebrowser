854a46c93194dfaec303a208a7bbafe6f4400815
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2025-08-31 20:34:13 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@f3e41457de3bfd4a6a44d54ca034cc28359b6462 🚀

---
+++ dev/src_wordfiles_POL_3-4W_Head_Sending_txt5cbc16bf4555e960ab9e.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_3-4W_Head_Sending_txt"],{
+
+/***/ "./src/wordfiles/POL_3-4W_Head_Sending.txt":
+/*!*************************************************!*\
+  !*** ./src/wordfiles/POL_3-4W_Head_Sending.txt ***!
+  \*************************************************/
+/***/ ((module) => {
+
+module.exports = "{|and} [   ] \r\n{|the} [   ] \r\n{|are} [   ] \r\n{|can} [   ] \r\n{|man} [   ] \r\n{|cat} [   ] \r\n{|dog} [   ] \r\n{|sun} [   ] \r\n{|run} [   ] \r\n{|car} [   ] \r\n{|pen} [   ] \r\n{|box} [   ] \r\n{|boy} [   ] \r\n{|toy} [   ] \r\n{|big} [   ] \r\n{|top} [   ] \r\n{|hat} [   ] \r\n{|red} [   ] \r\n{|eat} [   ] \r\n{|win} [   ] \r\n{|fan} [   ] \r\n{|job} [   ] \r\n{|leg} [   ] \r\n{|map} [   ] \r\n{|bed} [   ] \r\n{|bus} [   ] \r\n{|cup} [   ] \r\n{|key} [   ] \r\n{|low} [   ] \r\n{|new} [   ] \r\n{|old} [   ] \r\n{|fix} [   ] \r\n{|pay} [   ] \r\n{|zip} [   ] \r\n{|ice} [   ] \r\n{|hot} [   ] \r\n{|log} [   ] \r\n{|man} [   ] \r\n{|sea} [   ] \r\n{|sky} [   ] \r\n{|arm} [   ] \r\n{|ear} [   ] \r\n{|eye} [   ] \r\n{|oil} [   ] \r\n{|row} [   ] \r\n{|sit} [   ] \r\n{|tag} [   ] \r\n{|box} [   ] \r\n{|ink} [   ] \r\n{|net} [   ] \r\n{|Book} [     ] \r\n{|Hand} [     ] \r\n{|Game} [     ] \r\n{|Tree} [     ] \r\n{|Fish} [     ] \r\n{|Love} [     ] \r\n{|Word} [     ] \r\n{|Door} [     ] \r\n{|Star} [     ] \r\n{|Rain} [     ] \r\n{|Ball} [     ] \r\n{|Time} [     ] \r\n{|Fire} [     ] \r\n{|Gold} [     ] \r\n{|Moon} [     ] \r\n{|Road} [     ] \r\n{|Ship} [     ] \r\n{|King} [     ] \r\n{|Girl} [     ] \r\n{|Bird} [     ] \r\n{|Play} [     ] \r\n{|Food} [     ] \r\n{|Land} [     ] \r\n{|Snow} [     ] \r\n{|Wind} [     ] \r\n{|Song} [     ] \r\n{|Walk} [     ] \r\n{|Park} [     ] \r\n{|Farm} [     ] \r\n{|Bell} [     ] \r\n{|Home} [     ] \r\n{|Rock} [     ] \r\n{|Jump} [     ] \r\n{|Wave} [     ] \r\n{|Leaf} [     ] \r\n{|Milk} [     ] \r\n{|Bear} [     ] \r\n{|Shop} [     ] \r\n{|Book} [     ] \r\n{|Ring} [     ] \r\n{|Soft} [     ] \r\n{|Cold} [     ] \r\n{|Warm} [     ] \r\n{|Dark} [     ] \r\n{|Tall} [     ] \r\n{|Fast} [     ] \r\n{|Good} [     ] \r\n{|Kind} [     ] \r\n{|Open} [     ] \r\n{|Hope} [     ] \r\n\r\n";
+
+/***/ })
+
+}]);
