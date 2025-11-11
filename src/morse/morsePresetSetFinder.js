85bb7904f4cb50afed76bde300bc67053abce20d
Rob Siebens <robert@robertsiebens.com>
2025-11-10 16:07:10 -0800

    Made changes to add card grid and default card message.

--- src/morse/morsePresetSetFinder.js
+++ src/morse/morsePresetSetFinder.js
@@ -6,14 +6,17 @@ export class MorsePresetSetFileFinder {
   static getMorsePresetSetFile = (fileName, afterFound) => {
     switch (fileName) {
       // BEGINA
-      case 'ADV.json':
-        import('../presets/sets/ADV.json').then(({ default: x }) => afterFound({ found: true, data: x }))
+      case 'A.json':
+        import('../presets/sets/A.json').then(({ default: x }) => afterFound({ found: true, data: x }))
         break
       case 'ADV1.json':
         import('../presets/sets/ADV1.json').then(({ default: x }) => afterFound({ found: true, data: x }))
         break
       case 'ADV2.json':
         import('../presets/sets/ADV2.json').then(({ default: x }) => afterFound({ found: true, data: x }))
+        break
+      case 'ADV3.json':
+        import('../presets/sets/ADV3.json').then(({ default: x }) => afterFound({ found: true, data: x }))
         break
       case 'bc1.json':
         import('../presets/sets/bc1.json').then(({ default: x }) => afterFound({ found: true, data: x }))
@@ -24,14 +27,20 @@ export class MorsePresetSetFileFinder {
       case 'bc3.json':
         import('../presets/sets/bc3.json').then(({ default: x }) => afterFound({ found: true, data: x }))
         break
-      case 'int_12_16.json':
-        import('../presets/sets/int_12_16.json').then(({ default: x }) => afterFound({ found: true, data: x }))
+      case 'INT1.json':
+        import('../presets/sets/INT1.json').then(({ default: x }) => afterFound({ found: true, data: x }))
         break
-      case 'int_16_20.json':
-        import('../presets/sets/int_16_20.json').then(({ default: x }) => afterFound({ found: true, data: x }))
+      case 'INT2.json':
+        import('../presets/sets/INT2.json').then(({ default: x }) => afterFound({ found: true, data: x }))
         break
-      case 'int_1_12.json':
-        import('../presets/sets/int_1_12.json').then(({ default: x }) => afterFound({ found: true, data: x }))
+      case 'INT3.json':
+        import('../presets/sets/INT3.json').then(({ default: x }) => afterFound({ found: true, data: x }))
+        break
+      case 'POL.json':
+        import('../presets/sets/POL.json').then(({ default: x }) => afterFound({ found: true, data: x }))
+        break
+      case 'TTR+.json':
+        import('../presets/sets/TTR+.json').then(({ default: x }) => afterFound({ found: true, data: x }))
         break
         // BEGINB
       default:
