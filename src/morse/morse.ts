d76e85eadfdf5b68b329eb8103950fb0fafb570a
Randy Loeb <githubrandyloeb@randonloeb.com>
2025-12-16 18:17:58 -0500

    log call to shuffleWords

--- src/morse/morse.ts
+++ src/morse/morse.ts
@@ -253,6 +253,7 @@ export class MorseViewModel {
   }, this)
 
   shuffleWords = (fromLoopRestart:boolean = false) => {
+    console.log(`shuffleWords called, isShuffled:${this.isShuffled()}, fromLoopRestart:${fromLoopRestart}`)
     // if it's not currently shuffled, or we're in a loop, re-shuffle
     if (!this.isShuffled() || fromLoopRestart) {
       const hasPhrases = this.rawText().indexOf('\n') !== -1 && this.settings.misc.newlineChunking()
