1f79e9aa6dcdd1b3d4feff75e9630237c4228451
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2026-01-05 20:27:05 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@d760775a926dba663dbd74a33ec773d2da89e016 🚀

---
+++ dev/src_wordfiles_POL_WB_Pt2_txt46eaf2dfac9333405b33.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_WB_Pt2_txt"],{
+
+/***/ "./src/wordfiles/POL_WB_Pt2.txt":
+/*!**************************************!*\
+  !*** ./src/wordfiles/POL_WB_Pt2.txt ***!
+  \**************************************/
+/***/ ((module) => {
+
+module.exports = "{AAA |AAA |1}  {ALL |ALL |1}  {ALSO |ALSO |1}  {AMPS |AMPS |1}  {AFTER |AFTER |1}  {BBB |BBB |2}  {BAD |BAD |2}  {BIG |BIG |2}  {BACK |BACK |2}  {BRAIN |BRAIN |2}  {CCC |CCC |3}  {COME |COME |3}  {COULD |COULD |3}  {CLOUDY |CLOUDY |3}  {DDD |DDD |4}  {DESK |DESK |4}  {DOING |DOING |4}  {DIPOLE |DIPOLE |4}  {EEE |EEE |5}  {EVEN |EVEN |5}  {EVERY |EVERY |5}  {ENDFED |ENDFED |5}  {FFF |FFF |6}  {FER |FER |6}  {FROM |FROM |6}  {FIRST |FIRST |6}  {GGG |GGG |7}  {GIVE |GIVE |7}  {GOOD |GOOD |7}  {GREAT |GREAT |7}  {HHH |HHH |8}  {HIM |HIM |8}  {HAVE |HAVE |8}  {HOBBY |HOBBY |8}  {III |III |9}  {ITS |ITS |9}  {INTO |INTO |9}  {IDEAS |IDEAS |9}  {JJJ |JJJ |10}  {JUST |JUST |10}  {JOKES |JOKES |10}  {KKK |KKK |11}  {KNOW |KNOW |11}  {KNOCK |KNOCK |11}  {KEYPAD |KEYPAD |11}  {LLL |LLL |12}  {LIKE |LIKE |12}  {LARGE |LARGE |12}  {LITTLE |LITTLE |12}  {MMM |MMM |13}  {ME |ME |13}  {MAKE |MAKE |13}  {MOST |MOST |13}  {MOVED |MOVED |13}  {NNN |NNN |14}  {NOW |NOW |14}  {NAME |NAME |14}  {NIGHT |NIGHT |14}  {OOO |OOO |15}  {OUT |OUT |15}  {OFF |OFF |15}  {OHM |OHM |15}  {OVER |OVER |15}  {PPP |PPP |16}  {PICK |PICK |16}  {PAPER |PAPER |16}  {PADDLE |PADDLE |16}  {QQQ |QQQ |17}  {QUAD |QUAD |17}  {QUICK |QUICK |17}  {QUILTS |QUILTS |17}  {RRR |RRR |18}  {RAIN |RAIN |18}  {RADIO |RADIO |18}  {REPEAT |REPEAT |18}  {SSS |SSS |19}  {SOME |SOME |19}  {SENSE |SENSE |19}  {SIGNAL |SIGNAL |19}  {TTT |TTT |20}  {THEY |THEY |20}  {TAKE |TAKE |20}  {TIME |TIME |20}  {THEM |THEM |20}  {THEN |THEN |20}  {THAN |THAN |20}  {UUU |UUU |21}  {USE |USE |21}  {UNIT |UNIT |22}  {UNIFY |UNIFY |21}  {VVV |VVV |22}  {VOLT |VOLT |22}  {VOICE |VOICE |22}  {VERIFY |VERIFY |22}  {WWW |WWW |23}  {WELL |WELL |23}  {WHEN |WHEN |23}  {WILL |WILL |23}  {WITH |WITH |23}  {WORK |WORK |23}  {XXX |XXX |24}  {XCVR |XCVR |24}  {XEROX |XEROX |24}  {XENON |XENON |24}  {YYY |YYY |25}  {YOUR |YOUR |25}  {YAGIS |YAGIS |25}  {YOUNG |YOUNG |25}  {YIELD |YIELD |25}  {ZZZ |ZZZ |26}  {ZOOM |ZOOM |26}  {ZONE |ZONE |26}  {ZINC |ZINC |26}  {ZEBRA |ZEBRA |26} \r\n";
+
+/***/ })
+
+}]);
