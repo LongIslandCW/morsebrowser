a00307d90077cbfeca1e3c968ea2de172cbe7477
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2026-02-03 21:32:21 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@4b6dbb8a9220ce8e3a3850beeb1756b983aef9ba 🚀

---
+++ dev/src_wordfiles_POL_Numbers_txtecb7b52f67b0ea8e7976.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_Numbers_txt"],{
+
+/***/ "./src/wordfiles/POL_Numbers.txt":
+/*!***************************************!*\
+  !*** ./src/wordfiles/POL_Numbers.txt ***!
+  \***************************************/
+/***/ ((module) => {
+
+module.exports = "[ ]1[  ]2[  ]3[  ]4[  ]5[  ]6[  ]7[  ]8[  ]9[  ]0 \r\n111 \r\n222 \r\n333 \r\n444 \r\n555 \r\n666 \r\n777 \r\n888 \r\n999 \r\n000 \r\n[ ]123[   ]456[   ]789[   ]0 \r\n[ ]123[  ]456[  ]789[  ]0 \r\n[ ]12345[   ]67890 \r\n[ ]12345[  ]67890 ";
+
+/***/ })
+
+}]);
