846c7500843d86882fdfaeccdbfcaed8564108ec
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2026-03-12 20:39:04 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@bc0cdb32202fdbb966e2532c4ad3906c0872c50b 🚀

---
+++ src_wordfiles_POL_SENDING_TRI_txt4baf0a2d0d3d00f22d94.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_POL_SENDING_TRI_txt"],{
+
+/***/ "./src/wordfiles/POL_SENDING_TRI.txt":
+/*!*******************************************!*\
+  !*** ./src/wordfiles/POL_SENDING_TRI.txt ***!
+  \*******************************************/
+/***/ ((module) => {
+
+module.exports = "ABC \r\nDEF \r\nGHI \r\nJKL \r\nMNO \r\nPQR \r\nSTU \r\nVWX \r\nXYZ \r\nZYX \r\nXWV \r\nUTS \r\nRQP \r\nONM \r\nLKJ \r\nIHG \r\nFED \r\nCBA \r\n";
+
+/***/ })
+
+}]);
