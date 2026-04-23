a5eb4bfeb7dc664394618152a948a1d736b454c7
nolleto1 <103961372+nolleto1@users.noreply.github.com>
2026-04-23 21:05:56 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@373b7a5bc8ee0197a854aab0cea3a2c8b781f9ca 🚀

---
+++ dev/src_wordfiles_BC3_ARRL_Sections_txt93c6b1fc7c329a72d69e.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_BC3_ARRL_Sections_txt"],{
+
+/***/ "./src/wordfiles/BC3_ARRL_Sections.txt":
+/*!*********************************************!*\
+  !*** ./src/wordfiles/BC3_ARRL_Sections.txt ***!
+  \*********************************************/
+/***/ ((module) => {
+
+module.exports = "{CO|Colorado } \r\n{IA|Iowa } \r\n{KS|Kansas } \r\n{MN|Minnesota } \r\n{MO|Missouri } \r\n{ND|North Dakota } \r\n{NE|Nebraska } \r\n{SD|South Dakota } \r\n{CT|Connecticut } \r\n{EMA|Eastern Massachusetts } \r\n{ME|Maine } \r\n{NH|New Hampshire } \r\n{RI|Rhode Island } \r\n{VT|Vermont } \r\n{WMA|Western Massachusetts } \r\n{ENY|Eastern New York } \r\n{NLI|New York City-Long Island } \r\n{NNJ|Northern New Jersey } \r\n{NNY|Northern New York } \r\n{SNJ|Southern New Jersey } \r\n{WNY|Western New York } \r\n{DE|Delaware } \r\n{EPA|Eastern Pennsylvania } \r\n{MDC|Maryland-DC } \r\n{WPA|Western Pennsylvania } \r\n{AL|Alabama } \r\n{GA|Georgia } \r\n{KY|Kentucky } \r\n{NC|North Carolina } \r\n{NFL|Northern Florida } \r\n{PR|Puerto Rico } \r\n{SC|South Carolina } \r\n{SFL|Southern Florida } \r\n{TN|Tennessee } \r\n{VA|Virginia } \r\n{VI|US Virgin Islands } \r\n{WCF|West Central Florida } \r\n{AR|Arkansas } \r\n{LA|Louisiana } \r\n{MS|Mississippi } \r\n{NM|New Mexico } \r\n{NTX|North Texas } \r\n{OK|Oklahoma } \r\n{STX|South Texas } \r\n{WTX|West Texas } \r\n{EB|East Bay } \r\n{LAX|Los Angeles } \r\n{ORG|Orange } \r\n{PAC|Pacific } \r\n{SB|Santa Barbara } \r\n{SCV|Santa Clara Valley } \r\n{SDG|San Diego } \r\n{SF|San Francisco } \r\n{SJV|San Joaquin Valley } \r\n{SV|Sacramento Valley } \r\n{AK|Alaska } \r\n{AZ|Arizona } \r\n{EWA|Eastern Washington } \r\n{ID|Idaho } \r\n{MT|Montana } \r\n{NV|Nevada } \r\n{OR|Oregon } \r\n{UT|Utah } \r\n{WWA|Western Washington } \r\n{WY|Wyoming } \r\n{MI|Michigan } \r\n{OH|Ohio } \r\n{WV|West Virginia } \r\n{IL|Illinois } \r\n{IN|Indiana } \r\n{WI|Wisconsin } \r\n{AB|Alberta } \r\n{BC|British Columbia } \r\n{GH|Ontario Golden Horseshoe } \r\n{MB|Manitoba } \r\n{NB|New Brunswick } \r\n{NL|Newfoundland and Labrador } \r\n{NS|Nova Scotia } \r\n{ONE|Ontario East } \r\n{ONN|Ontario North } \r\n{ONS|Ontario South } \r\n{PE|Prince Edward Island } \r\n{QC|Quebec } \r\n{SK|Saskatchewan } \r\n{TER|Territories } \r\n";
+
+/***/ })
+
+}]);
