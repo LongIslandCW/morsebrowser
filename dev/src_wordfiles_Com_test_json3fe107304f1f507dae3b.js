a004177278b7534b5bb2a576d52fba465db74a44
RandyLoeb <34562250+RandyLoeb@users.noreply.github.com>
2026-07-06 20:40:22 +0000

    Deploying to gh-pages from @ LongIslandCW/morsebrowser@c9a4047765f6d778ebe198f8e3383168a1ef00e1 🚀

---
+++ dev/src_wordfiles_Com_test_json3fe107304f1f507dae3b.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_wordfiles_Com_test_json"],{
+
+/***/ "./src/wordfiles/Com_test.json":
+/*!*************************************!*\
+  !*** ./src/wordfiles/Com_test.json ***!
+  \*************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"letters":"abcdefghijklmnopqrstuvwxyz1234567890 <sk> <bt> / , . ? <ar> <bk> ","minWordSize":5,"maxWordSize":5,"practiceSeconds":120}');
+
+/***/ })
+
+}]);
